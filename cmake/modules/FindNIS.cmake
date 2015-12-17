7373a86388d9761672721751bd02711038c22c37
Ivailo Monev <xakepa10@gmail.com>
2015-12-17 05:17:14 +0200

    make network information service support optional as it was in qt
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

---
+++ cmake/modules/FindNIS.cmake
@@ -0,0 +1,38 @@
+# - Try to find Network Information Service
+# Once done this will define
+#
+#  NIS_FOUND - system has NIS
+#  NIS_INCLUDES - the NIS include directory
+#  NIS_LIBRARIES - The libraries needed to use NIS
+#
+# Copyright (c) 2015, Ivailo Monev, <xakepa10@gmail.com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+
+if(NIS_INCLUDES AND NIS_LIBRARIES)
+    set(NIS_FIND_QUIETLY TRUE)
+endif()
+
+find_path(NIS_INCLUDES
+    NAMES
+    rpc/rpc.h
+    HINTS
+    $ENV{NISDIR}/include
+    /usr/include
+    /usr/local/include
+    ${INCLUDE_INSTALL_DIR}
+)
+
+find_library(NIS_LIBRARIES
+    nsl
+    HINTS
+    $ENV{NISDIR}/lib
+    /usr/lib
+    /usr/local/lib
+    ${LIB_INSTALL_DIR}
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(NIS DEFAULT_MSG NIS_INCLUDES NIS_LIBRARIES)
+
+mark_as_advanced(NIS_INCLUDES NIS_LIBRARIES)
