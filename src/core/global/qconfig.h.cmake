340dc2cc5475a56f30ff6522e47aab522fb49f8a
Ivailo Monev <xakepa10@gmail.com>
2016-01-19 14:53:31 +0200

    define QT_LARGEFILE_SUPPORT as 64 when supported like Qt does it
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/core/global/qconfig.h.cmake
+++ src/core/global/qconfig.h.cmake
@@ -61,7 +61,7 @@
 #endif
 
 #cmakedefine QT_VISIBILITY_AVAILABLE
-#cmakedefine QT_LARGEFILE_SUPPORT
+#cmakedefine QT_LARGEFILE_SUPPORT ${QT_LARGEFILE_SUPPORT}
 #cmakedefine QT_POINTER_SIZE ${QT_POINTER_SIZE}
 
 /* Data structures */
