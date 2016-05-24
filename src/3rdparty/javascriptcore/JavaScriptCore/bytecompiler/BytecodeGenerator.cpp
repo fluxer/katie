ca645d278ca6a2b602208ee008e62b7dccef5374
Ivailo Monev <xakepa10@gmail.com>
2016-05-24 11:21:58 +0000

    fix some compiler warnings in 3rd party JavaScriptCore
    upstream commits:
    https://github.com/qtproject/qtscript/commit/a2da34be8426b41047f895a3e8463d58baf3b576
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/3rdparty/javascriptcore/JavaScriptCore/bytecompiler/BytecodeGenerator.cpp
+++ src/3rdparty/javascriptcore/JavaScriptCore/bytecompiler/BytecodeGenerator.cpp
@@ -1764,16 +1764,15 @@ RegisterID* BytecodeGenerator::emitNextPropertyName(RegisterID* dst, RegisterID*
 
 RegisterID* BytecodeGenerator::emitCatch(RegisterID* targetRegister, Label* start, Label* end)
 {
-#if ENABLE(JIT)
-    HandlerInfo info = { start->bind(0, 0), end->bind(0, 0), instructions().size(), m_dynamicScopeDepth + m_baseScopeDepth, CodeLocationLabel() };
-#else
     HandlerInfo info = {
         static_cast<uint32_t>(start->bind(0, 0)),
         static_cast<uint32_t>(end->bind(0, 0)),
         static_cast<uint32_t>(instructions().size()),
         static_cast<uint32_t>(m_dynamicScopeDepth + m_baseScopeDepth)
-    };
+#if ENABLE(JIT)
+        , CodeLocationLabel()
 #endif
+    };
 
     m_codeBlock->addExceptionHandler(info);
     emitOpcode(op_catch);
