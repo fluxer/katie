c1b6b57b684e5f9e37dd2b1923c07c7e10af8936
Ivailo Monev <xakepa10@gmail.com>
2016-06-08 07:30:10 +0000

    fix GCC 6 valid warnings about misleading indendations
    upstream commits:
    https://github.com/qtproject/qtbase/commit/157445e64a277098b309b2bdfeda769f443b659d
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/gui/widgets/qcombobox.cpp
+++ src/gui/widgets/qcombobox.cpp
@@ -2759,8 +2759,8 @@ bool QComboBox::event(QEvent *event)
     case QEvent::HoverEnter:
     case QEvent::HoverLeave:
     case QEvent::HoverMove:
-    if (const QHoverEvent *he = static_cast<const QHoverEvent *>(event))
-        d->updateHoverControl(he->pos());
+        if (const QHoverEvent *he = static_cast<const QHoverEvent *>(event))
+            d->updateHoverControl(he->pos());
         break;
     case QEvent::ShortcutOverride:
         if (d->lineEdit)
