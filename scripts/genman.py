4bed67615cac0893f4fa95e1c422cb76f1941055
Ivailo Monev <xakepa10@gmail.com>
2016-07-06 12:48:12 +0000

    cleanup scripts
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

---
+++ scripts/genman.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import sys, os, subprocess
+
+cwd = os.path.dirname(__file__)
+
+pod2man = None
+for path in os.environ.get('PATH', '/bin:/usr/bin').split(':'):
+    sfile = '%s/pod2man' % path
+    if os.access(sfile, os.X_OK):
+        pod2man = sfile
+        break
+
+if not pod2man:
+    sys.stderr.write('Perl is not installed\n')
+    sys.exit(1)
+
+sys.stdout.write('Generating man pages for Katie...\n')
+for root, sdir, lfiles in os.walk('%s/../src/tools' % cwd):
+    for sfile in lfiles:
+        if sfile.endswith('.pod'):
+            ffile = '%s/%s' % (root, sfile)
+            ofile = ffile.replace('.pod', '.1')
+            subprocess.check_call(
+                (pod2man,
+                '--release=Katie 4.9.0',
+                '--center=Katie Manual',
+                '--section=1',
+                '--utf8',
+                ffile,
+                ofile)
+            )
