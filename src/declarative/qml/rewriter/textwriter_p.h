70c94fbbab6104985436312aa2f528abb41be532
Ivailo Monev <xakepa10@gmail.com>
2016-07-11 10:45:16 +0000

    cleanup declerative rewriter, nothing does move operations
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/declarative/qml/rewriter/textwriter_p.h
+++ src/declarative/qml/rewriter/textwriter_p.h
@@ -54,7 +54,6 @@ namespace QDeclarativeJS {
 class TextWriter
 {
 	QString *string;
-	QTextCursor *cursor;
 
 	struct Replace {
 		int pos;
@@ -64,31 +63,14 @@ class TextWriter
 
 	QList<Replace> replaceList;
 
-	struct Move {
-		int pos;
-		int length;
-		int to;
-	};
-
-	QList<Move> moveList;
-
 	bool hasOverlap(int pos, int length);
-	bool hasMoveInto(int pos, int length);
-
-	void doReplace(const Replace &replace);
-	void doMove(const Move &move);
-
-	void write_helper();
 
 public:
 	TextWriter();
 
 	void replace(int pos, int length, const QString &replacement);
-	void move(int pos, int length, int to);
 
 	void write(QString *s);
-	void write(QTextCursor *textCursor);
-
 };
 
 } // end of namespace QDeclarativeJS
