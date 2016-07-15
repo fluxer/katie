3e4e6c427946250343edaf12db92683e4676832c
Ivailo Monev <xakepa10@gmail.com>
2016-07-15 16:08:45 +0000

    removed legacy pixmap and image cleanup hooks
    upstream commits:
    https://github.com/qt/qtbase/commit/8aa0be210a417d3e3955a3add5d57efe2565d774
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/gui/image/qimagepixmapcleanuphooks.cpp
+++ src/gui/image/qimagepixmapcleanuphooks.cpp
@@ -46,15 +46,6 @@
 
 QT_BEGIN_NAMESPACE
 
-// Legacy, single instance hooks: ### Qt 5: remove
-typedef void (*_qt_pixmap_cleanup_hook)(int);
-typedef void (*_qt_pixmap_cleanup_hook_64)(qint64);
-typedef void (*_qt_image_cleanup_hook)(int);
-Q_GUI_EXPORT _qt_pixmap_cleanup_hook qt_pixmap_cleanup_hook = 0;
-Q_GUI_EXPORT _qt_pixmap_cleanup_hook_64 qt_pixmap_cleanup_hook_64 = 0;
-Q_GUI_EXPORT _qt_image_cleanup_hook qt_image_cleanup_hook = 0;
-Q_GUI_EXPORT _qt_image_cleanup_hook_64 qt_image_cleanup_hook_64 = 0;
-
 Q_GLOBAL_STATIC(QImagePixmapCleanupHooks, qt_image_and_pixmap_cleanup_hooks)
 
 QImagePixmapCleanupHooks *QImagePixmapCleanupHooks::instance()
@@ -103,9 +94,6 @@ void QImagePixmapCleanupHooks::executePixmapDataModificationHooks(QPixmapData* p
         return;
     for (int i = 0; i < h->pixmapModificationHooks.count(); ++i)
         h->pixmapModificationHooks[i](pmd);
-
-    if (qt_pixmap_cleanup_hook_64)
-        qt_pixmap_cleanup_hook_64(pmd->cacheKey());
 }
 
 void QImagePixmapCleanupHooks::executePixmapDataDestructionHooks(QPixmapData* pmd)
@@ -118,18 +106,12 @@ void QImagePixmapCleanupHooks::executePixmapDataDestructionHooks(QPixmapData* pm
         return;
     for (int i = 0; i < h->pixmapDestructionHooks.count(); ++i)
         h->pixmapDestructionHooks[i](pmd);
-
-    if (qt_pixmap_cleanup_hook_64)
-        qt_pixmap_cleanup_hook_64(pmd->cacheKey());
 }
 
 void QImagePixmapCleanupHooks::executeImageHooks(qint64 key)
 {
     for (int i = 0; i < qt_image_and_pixmap_cleanup_hooks()->imageHooks.count(); ++i)
         qt_image_and_pixmap_cleanup_hooks()->imageHooks[i](key);
-
-    if (qt_image_cleanup_hook_64)
-        qt_image_cleanup_hook_64(key);
 }
 
 
