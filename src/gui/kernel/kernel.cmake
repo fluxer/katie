b0bb3c85de7e16206a88bb24c62ca9323d3991aa
Ivailo Monev <xakepa10@laimg.moc>
2016-07-25 13:19:47 +0000

    add required OS X header to GUI_HEADERS
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- src/gui/kernel/kernel.cmake
+++ src/gui/kernel/kernel.cmake
@@ -188,6 +188,7 @@ elseif(KATIE_PLATFORM STREQUAL "mac")
         ${CMAKE_CURRENT_SOURCE_DIR}/kernel/qcocoasharedwindowmethods_mac_p.h
         ${CMAKE_CURRENT_SOURCE_DIR}/kernel/qcocoaintrospection_p.h
         ${CMAKE_CURRENT_SOURCE_DIR}/kernel/qmacdefines_mac.h
+        ${CMAKE_CURRENT_SOURCE_DIR}/kernel/qt_mac_p.h
     )
     set(GUI_SOURCES
         ${GUI_SOURCES}
