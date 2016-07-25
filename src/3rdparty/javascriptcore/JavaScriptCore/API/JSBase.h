ff68aaeece0a2018d114a909dcdfc239d6f9c244
Ivailo Monev <xakepa10@laimg.moc>
2016-07-25 09:21:54 +0000

    minor JavaScriptCore/script component cleanup
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- src/3rdparty/javascriptcore/JavaScriptCore/API/JSBase.h
+++ src/3rdparty/javascriptcore/JavaScriptCore/API/JSBase.h
@@ -58,21 +58,9 @@ typedef const struct OpaqueJSValue* JSValueRef;
 /*! @typedef JSObjectRef A JavaScript object. A JSObject is a JSValue. */
 typedef struct OpaqueJSValue* JSObjectRef;
 
-/* JavaScript symbol exports */
+/* JavaScript symbol are not exported */
 
 #undef JS_EXPORT
-#if defined(JS_NO_EXPORT)
-    #define JS_EXPORT
-#elif defined(__GNUC__) && !defined(__CC_ARM) && !defined(__ARMCC__)
-    #define JS_EXPORT __attribute__((visibility("default")))
-#elif defined(WIN32) || defined(_WIN32) || defined(_WIN32_WCE)
-    #if defined(BUILDING_JavaScriptCore) || defined(BUILDING_WTF)
-        #define JS_EXPORT __declspec(dllexport)
-    #else
-        #define JS_EXPORT __declspec(dllimport)
-    #endif
-#else
-    #define JS_EXPORT
-#endif
+#define JS_EXPORT
 
 #endif /* JSBase_h */
