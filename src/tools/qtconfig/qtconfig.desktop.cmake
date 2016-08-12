54d6270593430ea50bd8ec54e7155a6a745eab4c
Ivailo Monev <xakepa10@laimg.moc>
2016-08-12 22:19:28 +0000

    add back qtconfig
    I'll find use for it when I implement antialiasing and image
    conversions options, maybe something else
    
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

---
+++ src/tools/qtconfig/qtconfig.desktop.cmake
@@ -0,0 +1,9 @@
+[Desktop Entry]
+Name=Katie Config
+Comment=Configure Katie behavior, styles, fonts
+Exec=@QT_BINARIES_PATH@/qtconfig
+Icon=@PIXMAPS_INSTALL_PATH@/qtconfig.png
+Terminal=false
+Encoding=UTF-8
+Type=Application
+Categories=Qt;Settings;
