f1ee06a269c5b3eb2c4f3871074094b843f93785
Ivailo Monev <xakepa10@laimg.moc>
2016-09-19 20:23:52 +0000

    mkspecs cleanup
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- mkspecs/freebsd/freebsd.cmake
+++ mkspecs/freebsd/freebsd.cmake
@@ -1,18 +1,1 @@
-if("${KATIE_COMPILER}" MATCHES "(gcc|clang)")
-    set(QT_VISIBILITY_AVAILABLE TRUE)
-endif()
-
 add_definitions(-D_THREAD_SAFE)
-
-set(QT_PREFIX_PATH "${CMAKE_INSTALL_PREFIX}")
-set(QT_HEADERS_PATH "${QT_PREFIX_PATH}/include/katie")
-set(QT_LIBRARIES_PATH "${QT_PREFIX_PATH}/lib/katie")
-set(QT_BINARIES_PATH "${QT_LIBRARIES_PATH}/bin")
-set(QT_PLUGINS_PATH "${QT_LIBRARIES_PATH}/plugins")
-set(QT_IMPORTS_PATH "${QT_LIBRARIES_PATH}/imports")
-set(QT_DATA_PATH "${QT_PREFIX_PATH}/share")
-set(QT_TRANSLATIONS_PATH "${QT_DATA_PATH}/translations")
-set(QT_DOCUMENTATION_PATH "${QT_DATA_PATH}/doc")
-set(QT_EXAMPLES_PATH "${QT_DATA_PATH}/examples")
-set(QT_DEMOS_PATH "${QT_DATA_PATH}/demos")
-set(QT_SETTINGS_PATH "/etc/xdg")
