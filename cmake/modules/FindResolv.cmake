c0edcb7c2c9c787ad6b40dd0dde01729ceeadf1b
Ivailo Monev <xakepa10@laimg.moc>
2016-10-16 21:13:18 +0000

    add Resolv CMake module
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

---
+++ cmake/modules/FindResolv.cmake
@@ -0,0 +1,41 @@
+# - Try to find resolv
+# Once done this will define
+#
+#  RESOLV_FOUND - system has resolv
+#  RESOLV_INCLUDES - the resolv include directory
+#  RESOLV_LIBRARIES - The libraries needed to use resolv
+#
+# Copyright (c) 2016, Ivailo Monev, <xakepa10@gmail.com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+
+if(RESOLV_INCLUDES AND RESOLV_LIBRARIES)
+    set(RESOLV_FIND_QUIETLY TRUE)
+endif()
+
+# resolv does not provide pkg-config files
+
+find_path(RESOLV_INCLUDES
+    NAMES
+    resolv.h
+    HINTS
+    $ENV{RESOLVDIR}/include
+    /usr/include
+    /usr/local/include
+    ${INCLUDE_INSTALL_DIR}
+)
+
+find_library(RESOLV_LIBRARIES
+    NAMES
+    resolv  c
+    HINTS
+    $ENV{RESOLVDIR}/lib
+    /usr/lib
+    /usr/local/lib
+    ${LIB_INSTALL_DIR}
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Resolv DEFAULT_MSG RESOLV_INCLUDES RESOLV_LIBRARIES)
+
+mark_as_advanced(RESOLV_INCLUDES RESOLV_LIBRARIES)
