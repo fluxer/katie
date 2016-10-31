42d41cacbad9594dd735e4f2b1957e9227bd37a2
Ivailo Monev <xakepa10@laimg.moc>
2016-10-31 23:03:04 +0000

    Revert "make default mode auto for QDBusConnection::call()"
    This reverts commit 230a9e2a574df4b747f5ea670497faed9e0d65bf.

--- src/dbus/qdbusconnection.h
+++ src/dbus/qdbusconnection.h
@@ -139,7 +139,7 @@ public:
                           int timeout = -1) const;
     bool callWithCallback(const QDBusMessage &message, QObject *receiver,
                           const char *slot, int timeout = -1) const;
-    QDBusMessage call(const QDBusMessage &message, QDBus::CallMode mode = QDBus::AutoDetect,
+    QDBusMessage call(const QDBusMessage &message, QDBus::CallMode mode = QDBus::Block,
                       int timeout = -1) const;
     QDBusPendingCall asyncCall(const QDBusMessage &message, int timeout = -1) const;
 
