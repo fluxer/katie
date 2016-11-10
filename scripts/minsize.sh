9ac104dbc4acd07189198fdbe80b5ab196578250
Ivailo Monev <xakepa10@laimg.moc>
2016-11-10 20:09:57 +0000

    add script to build with LTO and MinSizeRel quickly
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

---
+++ scripts/minsize.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+cwd="$(dirname $0)"
+
+mkdir -p "$cwd/../minsize"
+cd "$cwd/../minsize"
+
+export CFLAGS="$CFLAGS -flto" CXXFLAGS="$CXXFLAGS -flto"
+
+cmake ../ -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_PREFIX=/usr
+make -j$(nproc)
