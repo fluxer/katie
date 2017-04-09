587dae58ad3d6e2dcef202ba74106f546a94e6f9
Ivailo Monev <xakepa10@laimg.moc>
2017-04-09 02:26:15 +0000

    micro-optimize QKeySequence::toString()
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- src/gui/kernel/qkeysequence.cpp
+++ src/gui/kernel/qkeysequence.cpp
@@ -1334,12 +1334,12 @@ QString QKeySequence::toString(SequenceFormat format) const
     QString finalString;
     // A standard string, with no translation or anything like that. In some ways it will
     // look like our latin case on Windows and X11
-    int end = count();
+    const int end = count();
     for (int i = 0; i < end; ++i) {
         finalString += d->encodeString(d->key[i], format);
-        finalString += QLatin1String(", ");
+        if (i < end)
+            finalString += QLatin1String(", ");
     }
-    finalString.truncate(finalString.length() - 2);
     return finalString;
 }
 
