8d81de7cdebf97294123d661b1bfba319283bc9f
Ivailo Monev <xakepa10@laimg.moc>
2017-08-09 19:40:40 +0000

    make it possible to test the imports too
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- cmake/runtest.sh.cmake
+++ cmake/runtest.sh.cmake
@@ -5,6 +5,7 @@ set -e
 export LD_LIBRARY_PATH="@CMAKE_BINARY_DIR@/lib"
 export QTDIR="@CMAKE_SOURCE_DIR@"
 export QT_PLUGIN_PATH="@CMAKE_BINARY_DIR@/plugins"
+export QML_IMPORT_PATH="@CMAKE_BINARY_DIR@/imports"
 
 cd "$(dirname "$1")"
 exec "./$(basename "$1")"
