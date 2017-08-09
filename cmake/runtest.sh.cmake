8c12d50cccb747917f9ea8d7ddc5fc9c38f1dc81
Ivailo Monev <xakepa10@laimg.moc>
2017-08-09 06:16:35 +0000

    various cleanups
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- cmake/runtest.sh.cmake
+++ cmake/runtest.sh.cmake
@@ -6,5 +6,5 @@ export LD_LIBRARY_PATH="@CMAKE_BINARY_DIR@/lib"
 export QTDIR="@CMAKE_SOURCE_DIR@"
 export QT_PLUGIN_PATH="@CMAKE_BINARY_DIR@/plugins"
 
-cd "$(dirname $1)"
-exec "./$(basename $1)"
+cd "$(dirname "$1")"
+exec "./$(basename "$1")"
--- scripts/fastdev.sh
+++ scripts/fastdev.sh
@@ -8,5 +8,5 @@ rm -rf "$cwd/../fastdev"
 mkdir -p "$cwd/../fastdev"
 cd "$cwd/../fastdev"
 
-cmake ../ -DCMAKE_BUILD_TYPE=FastDev -DCMAKE_INSTALL_PREFIX=/usr $@
+cmake ../ -DCMAKE_BUILD_TYPE=FastDev -DKATIE_ALLINONE=ON -DCMAKE_INSTALL_PREFIX=/usr $@
 make -j$(nproc)
