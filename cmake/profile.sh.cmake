8763b4ebf716355a49fccae20daa5f6bf2a14d5e
Ivailo Monev <xakepa10@laimg.moc>
2017-08-22 16:31:32 +0000

    improve the shell profile file
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- cmake/profile.sh.cmake
+++ cmake/profile.sh.cmake
@@ -1,15 +1,29 @@
 #!/bin/sh
 
-if [ -n "$LD_LIBRARY_PATH" ];then
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:@KATIE_LIBRARIES_FULL@"
-else
-    export LD_LIBRARY_PATH="@KATIE_LIBRARIES_FULL@"
-fi
+# NOTE: safe as in does not append if already in the variable
+safe_path_append() {
+    oldIFS="$IFS"
+    IFS=":"
+    found=""
+    for i in $2;do
+        # TODO: maybe check for match without trailing slash too?
+        if [ "$i" = "$3" ];then
+            found="yes"
+        fi
+    done
+    IFS="$oldIFS"
 
-if [ -n "$PKG_CONFIG_PATH" ];then
-    export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:@KATIE_PKGCONFIG_FULL@"
-else
-    export PKG_CONFIG_PATH="@KATIE_PKGCONFIG_FULL@"
-fi
+    if [ "$found" != "yes" ];then
+        export "$1"="$2:$3"
+    fi
+}
+
+safe_path_append PATH "$PATH" "@KATIE_BINARIES_FULL@"
+
+safe_path_append LD_LIBRARY_PATH "$LD_LIBRARY_PATH" "@KATIE_LIBRARIES_FULL@"
+
+safe_path_append PKG_CONFIG_PATH "$PKG_CONFIG_PATH" "@KATIE_PKGCONFIG_FULL@"
 
 export Katie_DIR="@KATIE_CMAKE_FULL@"
+
+unset -f safe_path_append
