1a31a54c9bda135b22f8bc2cf8eeb48beb0ba4ac
Ivailo Monev <xakepa10@laimg.moc>
2019-05-06 20:39:55 +0000

    set paths suitable for installation when building from scripts
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- scripts/fastdev.sh
+++ scripts/fastdev.sh
@@ -8,5 +8,9 @@ rm -rf "$cwd/../fastdev"
 mkdir -p "$cwd/../fastdev"
 cd "$cwd/../fastdev"
 
-cmake ../ -DCMAKE_BUILD_TYPE=FastDev $@
+cmake ../ -DCMAKE_BUILD_TYPE=FastDev \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DKATIE_SETTINGS_FULL=/etc/xdg \
+    -DKATIE_LDCONF_FULL=/etc/ld.so.conf.d \
+    -DKATIE_PROFILE_FULL=/etc/profile.d $@
 make -j$(nproc || echo 1)
--- scripts/minsize.sh
+++ scripts/minsize.sh
@@ -10,5 +10,9 @@ cd "$cwd/../minsize"
 
 export CFLAGS="$CFLAGS -flto" CXXFLAGS="$CXXFLAGS -flto"
 
-cmake ../ -DCMAKE_BUILD_TYPE=MinSizeRel $@
+cmake ../ -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DKATIE_SETTINGS_FULL=/etc/xdg \
+    -DKATIE_LDCONF_FULL=/etc/ld.so.conf.d \
+    -DKATIE_PROFILE_FULL=/etc/profile.d $@
 make -j$(nproc || echo 1)
