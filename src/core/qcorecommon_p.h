24e336a178e779b357e2dae18d4ad6a6587ad3ff
Ivailo Monev <xakepa10@laimg.moc>
2019-05-14 18:10:54 +0000

    put common core helper functions in non-anonymous namespace and fix compiler warning
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- src/core/qcorecommon_p.h
+++ src/core/qcorecommon_p.h
@@ -6,28 +6,26 @@
 QT_BEGIN_NAMESPACE
 
 #if !defined(QT_NO_THREAD) && defined(_POSIX_THREAD_SAFE_FUNCTIONS) && _POSIX_VERSION >= 200112L
-namespace {
-    // There are two incompatible versions of strerror_r:
-    // a) the XSI/POSIX.1 version, which returns an int,
-    //    indicating success or not
-    // b) the GNU version, which returns a char*, which may or may not
-    //    be the beginning of the buffer we used
-    // The GNU libc manpage for strerror_r says you should use the the XSI
-    // version in portable code. However, it's impossible to do that if
-    // _GNU_SOURCE is defined so we use C++ overloading to decide what to do
-    // depending on the return type
-    static inline QString fromstrerror_helper(int, const QByteArray &buf)
-    {
-        return QString::fromLocal8Bit(buf.constData());
-    }
-    static inline QString fromstrerror_helper(const char *str, const QByteArray &)
-    {
-        return QString::fromLocal8Bit(str);
-    }
+// There are two incompatible versions of strerror_r:
+// a) the XSI/POSIX.1 version, which returns an int,
+//    indicating success or not
+// b) the GNU version, which returns a char*, which may or may not
+//    be the beginning of the buffer we used
+// The GNU libc manpage for strerror_r says you should use the the XSI
+// version in portable code. However, it's impossible to do that if
+// _GNU_SOURCE is defined so we use C++ overloading to decide what to do
+// depending on the return type
+static inline QString fromstrerror_helper(int, const QByteArray &buf)
+{
+    return QString::fromLocal8Bit(buf.constData());
+}
+static inline QString fromstrerror_helper(const char *str, const QByteArray &)
+{
+    return QString::fromLocal8Bit(str);
 }
 #endif
 
-static void report_error(int code, const char *where, const char *what)
+static inline void report_error(int code, const char *where, const char *what)
 {
     if (code != 0)
         qWarning("%s: %s failure: %s", where, what, qPrintable(qt_error_string(code)));
