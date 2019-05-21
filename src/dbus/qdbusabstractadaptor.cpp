fluxer/katie

commit a133aaf3a841f451a277587cccf578fc624a6054

apply upstream commits  https://github.com/qt/qtbase/commit/c619fc278d7e5acd8f0ae8f9e2013f351c50c355  and  https://github.com/qt/qtbase/commit/4dcd9145e74dfb8c59016cf0011987cdeb6b3576
Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

src/tools/moc/generator.cpp
@@ -375,8 +375,8 @@ void Generator::generateCode()
         if (cdef->superclassList.at(i).second == FunctionDef::Private)
             continue;
         const char *cname = cdef->superclassList.at(i).first.constData();
-        fprintf(out, "    if (!strcmp(_clname, \"%s\"))\n        return static_cast< %s*>(const_cast< %s*>(this));\n",
-                cname, cname, cdef->classname.constData());
+        fprintf(out, "    if (!strcmp(_clname, \"%s\"))\n        return static_cast< %s*>(this);\n",
+                cname, cdef->classname.constData());
     }
     for (int i = 0; i < cdef->interfaceList.size(); ++i) {
         const QList<ClassDef::Interface> &iface = cdef->interfaceList.at(i);
@@ -384,8 +384,7 @@ void Generator::generateCode()
             fprintf(out, "    if (!strcmp(_clname, %s))\n        return ", iface.at(j).interfaceId.constData());
             for (int k = j; k >= 0; --k)
                 fprintf(out, "static_cast< %s*>(", iface.at(k).className.constData());
-            fprintf(out, "const_cast< %s*>(this)%s;\n",
-                    cdef->classname.constData(), QByteArray(j+1, ')').constData());
+            fprintf(out, "this%s;\n", QByteArray(j+1, ')').constData());
         }
     }
     if (!purestSuperClass.isEmpty() && !isQObject) {

src/tools/moc/moc.cpp
@@ -364,8 +364,8 @@ bool Moc::parseFunction(FunctionDef *def, bool inMacro)
     def->isVirtual = false;
     def->isStatic = false;
     //skip modifiers and attributes
-    while (test(INLINE) || (test(STATIC) && (def->isStatic = true)) ||
-        (test(VIRTUAL) && (def->isVirtual = true)) //mark as virtual
+    while (test(INLINE) || (test(STATIC) && (def->isStatic = true) == true) ||
+        (test(VIRTUAL) && (def->isVirtual = true) == true) //mark as virtual
         || testFunctionAttribute(def) || testFunctionRevision(def)) {}
     bool templateFunction = (lookup() == TEMPLATE);
     def->type = parseType();
@@ -456,8 +456,8 @@ bool Moc::parseMaybeFunction(const ClassDef *cdef, FunctionDef *def)
     def->isVirtual = false;
     def->isStatic = false;
     //skip modifiers and attributes
-    while (test(EXPLICIT) || test(INLINE) || (test(STATIC) && (def->isStatic = true)) ||
-        (test(VIRTUAL) && (def->isVirtual = true)) //mark as virtual
+    while (test(EXPLICIT) || test(INLINE) || (test(STATIC) && (def->isStatic = true) == true) ||
+        (test(VIRTUAL) && (def->isVirtual = true) == true) //mark as virtual
         || testFunctionAttribute(def) || testFunctionRevision(def)) {}
     bool tilde = test(TILDE);
     def->type = parseType();

commit 6df8910c7dda84917541daa14193cfe673bd2383

fix DBus component regression
the regression is since
https://github.com/fluxer/katie/commit/a133aaf3a841f451a277587cccf578fc624a6054

the follwoing commits are related to it:
https://github.com/fluxer/katie/commit/40ad51d158a07a460ebfa210641e8bbb06f6333b
https://github.com/fluxer/katie/commit/95580728f0224d4d09646d8ce9ba9b06e39beec8

Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

src/dbus/qdbusabstractadaptor.cpp
@@ -391,7 +391,7 @@ void *QDBusAdaptorConnector::qt_metacast(const char *_clname)
 {
     if (!_clname) return Q_NULLPTR;
     if (!strcmp(_clname, qt_meta_stringdata_QDBusAdaptorConnector))
-        return static_cast<void*>(this);
+        return static_cast<void*>(const_cast< QDBusAdaptorConnector*>(this));
     return QObject::qt_metacast(_clname);
 }
 

src/dbus/qdbuscontext.cpp
@@ -57,8 +57,6 @@ QDBusContextPrivate *QDBusContextPrivate::set(QObject *obj, QDBusContextPrivate 
 
     Q_ASSERT(obj);
 
-#warning noop
-#if 0
     void *ptr = obj->qt_metacast("QDBusContext");
     QDBusContext *q_ptr = reinterpret_cast<QDBusContext *>(ptr);
     if (q_ptr) {
@@ -66,7 +64,6 @@ QDBusContextPrivate *QDBusContextPrivate::set(QObject *obj, QDBusContextPrivate 
         q_ptr->d_ptr = newContext;
         return old;
     }
-#endif
 
     return Q_NULLPTR;
 }

src/tools/moc/generator.cpp
@@ -374,8 +374,8 @@ void Generator::generateCode()
         if (cdef->superclassList.at(i).second == FunctionDef::Private)
             continue;
         const char *cname = cdef->superclassList.at(i).first.constData();
-        fprintf(out, "    if (!strcmp(_clname, \"%s\"))\n        return static_cast< %s*>(this);\n",
-                cname, cdef->classname.constData());
+        fprintf(out, "    if (!strcmp(_clname, \"%s\"))\n        return static_cast< %s*>(const_cast< %s*>(this));\n",
+                cname, cname, cdef->classname.constData());
     }
     for (int i = 0; i < cdef->interfaceList.size(); ++i) {
         const QList<ClassDef::Interface> &iface = cdef->interfaceList.at(i);
@@ -383,7 +383,8 @@ void Generator::generateCode()
             fprintf(out, "    if (!strcmp(_clname, %s))\n        return ", iface.at(j).interfaceId.constData());
             for (int k = j; k >= 0; --k)
                 fprintf(out, "static_cast< %s*>(", iface.at(k).className.constData());
-            fprintf(out, "this%s;\n", QByteArray(j+1, ')').constData());
+            fprintf(out, "const_cast< %s*>(this)%s;\n",
+                    cdef->classname.constData(), QByteArray(j+1, ')').constData());
         }
     }
     if (!purestSuperClass.isEmpty() && !isQObject) {