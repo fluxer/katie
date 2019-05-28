34e581e79f169ade94ffad1ba810f35b91473965
Ivailo Monev <xakepa10@laimg.moc>
2019-05-28 12:50:19 +0000

    make it easier to test old CMake versions on platforms other than i386 Linux [ci skip]
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- scripts/oldmake.sh
+++ scripts/oldmake.sh
@@ -3,22 +3,23 @@
 set -e
 
 cwd="$(realpath $(dirname $0))"
+cmake="cmake-2.8.12.2-Linux-i386"
 
 cd "$cwd"
 if which wget; then
-    wget -c https://cmake.org/files/v2.8/cmake-2.8.12.2-Linux-i386.tar.gz
+    wget -c "https://cmake.org/files/v2.8/$cmake.tar.gz"
 else
-    curl -C - https://cmake.org/files/v2.8/cmake-2.8.12.2-Linux-i386.tar.gz -o cmake-2.8.12.2-Linux-i386.tar.gz
+    curl -C - "https://cmake.org/files/v2.8/$cmake.tar.gz" -o "$cmake.tar.gz"
 fi
 
 rm -rf "$cwd/../oldmake"
 mkdir -p "$cwd/../oldmake"
 cd "$cwd/../oldmake"
 
-tar -xaf "$cwd/cmake-2.8.12.2-Linux-i386.tar.gz"
-cmake-2.8.12.2-Linux-i386/bin/cmake ../ -DCMAKE_BUILD_TYPE=Release -DENABLE_TESTING=TRUE -DKATIE_UTILS=ON $@
+tar -xaf "$cwd/$cmake.tar.gz"
+"$cmake/bin/cmake" ../ -DCMAKE_BUILD_TYPE=Release -DENABLE_TESTING=TRUE -DKATIE_UTILS=ON $@
 rm -rf *
 
-tar -xaf "$cwd/cmake-2.8.12.2-Linux-i386.tar.gz"
-cmake-2.8.12.2-Linux-i386/bin/cmake ../ -DCMAKE_BUILD_TYPE=Release -DENABLE_TESTING=TRUE -DKATIE_UTILS=ON -DKATIE_ALLINONE=ON $@
+tar -xaf "$cwd/$cmake.tar.gz"
+"$cmake/bin/cmake" ../ -DCMAKE_BUILD_TYPE=Release -DENABLE_TESTING=TRUE -DKATIE_UTILS=ON -DKATIE_ALLINONE=ON $@
 # not making on purpose
