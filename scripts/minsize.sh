8d4f632081e5f5fbd9cf7033b45b661adf41d50c
Ivailo Monev <xakepa10@laimg.moc>
2019-06-07 14:00:29 +0000

    make minsize script build without exceptions
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- scripts/minsize.sh
+++ scripts/minsize.sh
@@ -8,11 +8,12 @@ rm -rf "$cwd/../minsize"
 mkdir -p "$cwd/../minsize"
 cd "$cwd/../minsize"
 
-export CFLAGS="$CFLAGS -flto" CXXFLAGS="$CXXFLAGS -flto"
+export CFLAGS="$CFLAGS -flto -fno-exceptions" CXXFLAGS="$CXXFLAGS -flto -fno-exceptions"
 
 cmake ../ -DCMAKE_BUILD_TYPE=MinSizeRel \
     -DCMAKE_INSTALL_PREFIX=/usr \
     -DKATIE_SETTINGS_FULL=/etc/xdg \
     -DKATIE_LDCONF_FULL=/etc/ld.so.conf.d \
-    -DKATIE_PROFILE_FULL=/etc/profile.d $@
+    -DKATIE_PROFILE_FULL=/etc/profile.d \
+    -DQT_NO_EXCEPTIONS $@
 make -j$(nproc || echo 1)
