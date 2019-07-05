911a16a4947f25ab9e709da0a21f6b955f610571
Ivailo Monev <xakepa10@laimg.moc>
2019-07-05 01:12:29 +0000

    attempt to workound the tests timeout in Travis
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- cmake/runtest.sh.cmake
+++ cmake/runtest.sh.cmake
@@ -12,5 +12,11 @@ shift
 
 export CTEST_OUTPUT_ON_FAILURE=1
 
+# 9 minutes timeout for the tests, Travis timeouts on 10min if there is no
+# output and I do not want some tests to be skipped because of one test. Like
+# the qpainter test - it does a lot of tests and can take more then 10min.
+export CTEST_TEST_TIMEOUT=540
+
+
 cd "$(dirname "$bin")"
 exec "./$(basename "$bin")" $@
