f89e2cb32c3da6fc265b6d8e6b31e48d7d46ae20
Ivailo Monev <xakepa10@laimg.moc>
2019-07-12 18:35:44 +0000

    Revert "avoid two icon cache lookups"
    This reverts commit 62f75b51f33846d4feca4f200d353cb8e0d03f7a.

--- src/gui/image/qicon.cpp
+++ src/gui/image/qicon.cpp
@@ -159,14 +159,13 @@ static QPixmapIconEngineEntry *bestSizeMatch( const QSize &size, QPixmapIconEngi
 QPixmapIconEngineEntry *QPixmapIconEngine::tryMatch(const QSize &size, QIcon::Mode mode, QIcon::State state)
 {
     QPixmapIconEngineEntry *pe = 0;
-    for (int i = 0; i < pixmaps.count(); ++i) {
+    for (int i = 0; i < pixmaps.count(); ++i)
         if (pixmaps.at(i).mode == mode && pixmaps.at(i).state == state) {
             if (pe)
                 pe = bestSizeMatch(size, &pixmaps[i], pe);
             else
                 pe = &pixmaps[i];
         }
-    }
     return pe;
 }
 
@@ -895,8 +894,9 @@ QIcon QIcon::fromTheme(const QString &name, const QIcon &fallback)
         return icon;
     }
 
-    icon = *qtIconCache()->object(name);
-    if (icon.isNull()) {
+    if (qtIconCache()->contains(name)) {
+        icon = *qtIconCache()->object(name);
+    } else {
         QIcon *cachedIcon  = new QIcon(new QIconLoaderEngine(name));
         qtIconCache()->insert(name, cachedIcon);
         icon = *cachedIcon;
