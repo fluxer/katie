b18137f47e005623f007da720d0adfd9bdc1d4fb
Ivailo Monev <xakepa10@laimg.moc>
2019-12-13 03:32:27 +0000

    rebrand generated SVG documents title and description
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- src/svg/qsvggenerator.cpp
+++ src/svg/qsvggenerator.cpp
@@ -94,8 +94,8 @@ public:
         outputDevice = 0;
         resolution = 72;
 
-        attributes.document_title = QLatin1String("Qt Svg Document");
-        attributes.document_description = QLatin1String("Generated with Qt");
+        attributes.document_title = QLatin1String("Katie Svg Document");
+        attributes.document_description = QLatin1String("Generated with Katie");
         attributes.font_family = QLatin1String("serif");
         attributes.font_size = QLatin1String("10pt");
         attributes.font_style = QLatin1String("normal");
