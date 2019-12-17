645d7550062657bc381515c589bc0a53569d4241
Ivailo Monev <xakepa10@laimg.moc>
2019-12-17 16:30:16 +0000

    add field code to Exec key in designer desktop file
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- src/tools/designer/designer.desktop.cmake
+++ src/tools/designer/designer.desktop.cmake
@@ -1,7 +1,7 @@
 [Desktop Entry]
 Name=Katie Designer
 Comment=Design GUIs for Katie applications
-Exec=@KATIE_BINARIES_FULL@/designer@KATIE_TOOLS_SUFFIX@@CMAKE_EXECUTABLE_SUFFIX@
+Exec=@KATIE_BINARIES_FULL@/designer@KATIE_TOOLS_SUFFIX@@CMAKE_EXECUTABLE_SUFFIX@ %F
 Icon=@KATIE_PIXMAPS_FULL@/designer@KATIE_TOOLS_SUFFIX@.png
 MimeType=application/x-designer;
 Terminal=false
