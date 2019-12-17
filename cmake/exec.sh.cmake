e99b981a62fc26bde321979d81cc193238dab61c
Ivailo Monev <xakepa10@laimg.moc>
2019-12-17 16:27:54 +0000

    do not assume LD_LIBRARY_PATH is not set in exec.sh script
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- cmake/exec.sh.cmake
+++ cmake/exec.sh.cmake
@@ -2,7 +2,11 @@
 
 set -e
 
-export LD_LIBRARY_PATH="@CMAKE_BINARY_DIR@/lib"
+if [ -z "$LD_LIBRARY_PATH" ];then
+    export LD_LIBRARY_PATH="@CMAKE_BINARY_DIR@/lib"
+else
+    export LD_LIBRARY_PATH="@CMAKE_BINARY_DIR@/lib:$LD_LIBRARY_PATH"
+fi
 export QTDIR="@CMAKE_SOURCE_DIR@"
 export QT_PLUGIN_PATH="@CMAKE_BINARY_DIR@/plugins"
 export QML_IMPORT_PATH="@CMAKE_BINARY_DIR@/imports"
