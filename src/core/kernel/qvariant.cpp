fluxer/katie

commit 71bd691d09470882be77dfa51f1509362c196d6b

introduce QVariant::Float as alias to QMetaType::Float
Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

src/core/kernel/qvariant.cpp
@@ -495,7 +495,7 @@ static bool compare(const QVariant::Private *a, const QVariant::Private *b)
     const void *b_ptr = b->is_shared ? b->data.shared->ptr : &(b->data.ptr);
 
     /* The reason we cannot place this test in a case branch above for the types
-     * QMetaType::VoidStar, QMetaType::QObjectStar and so forth, is that it wouldn't include
+     * QMetaType::VoidStar, QMetaType::QObjectStar and so forth, as that wouldn't include
      * user defined pointer types. */
     const char *const typeName = QMetaType::typeName(a->type);
     uint typeNameLen = qstrlen(typeName);
@@ -1157,7 +1157,7 @@ static void streamDebug(QDebug dbg, const QVariant &v)
     case QVariant::ULongLong:
         dbg.nospace() << v.toULongLong();
         break;
-    case QMetaType::Float:
+    case QVariant::Float:
         dbg.nospace() << v.toFloat();
         break;
     case QMetaType::QObjectStar:
@@ -1791,6 +1791,8 @@ QVariant::QVariant(bool val)
 { d.is_null = false; d.type = Bool; d.data.b = val; }
 QVariant::QVariant(double val)
 { d.is_null = false; d.type = Double; d.data.d = val; }
+QVariant::QVariant(float val)
+{ d.is_null = false; d.type = Float; d.data.f = val; }
 
 QVariant::QVariant(const QByteArray &val)
 { d.is_null = false; d.type = ByteArray; v_construct<QByteArray>(&d, val); }
@@ -2612,7 +2614,7 @@ bool QVariant::toBool() const
 
 /*!
     Returns the variant as a double if the variant has type() \l
-    Double, \l QMetaType::Float, \l Bool, \l ByteArray, \l Int, \l LongLong, \l String, \l
+    Double, \l Float, \l Bool, \l ByteArray, \l Int, \l LongLong, \l String, \l
     UInt, or \l ULongLong; otherwise returns 0.0.
 
     If \a ok is non-null: \c{*}\a{ok} is set to true if the value could be
@@ -2627,7 +2629,7 @@ double QVariant::toDouble(bool *ok) const
 
 /*!
     Returns the variant as a float if the variant has type() \l
-    Double, \l QMetaType::Float, \l Bool, \l ByteArray, \l Int, \l LongLong, \l String, \l
+    Double, \l Float, \l Bool, \l ByteArray, \l Int, \l LongLong, \l String, \l
     UInt, or \l ULongLong; otherwise returns 0.0.
 
     \since 4.6
@@ -2644,7 +2646,7 @@ float QVariant::toFloat(bool *ok) const
 
 /*!
     Returns the variant as a qreal if the variant has type() \l
-    Double, \l QMetaType::Float, \l Bool, \l ByteArray, \l Int, \l LongLong, \l String, \l
+    Double, \l Float, \l Bool, \l ByteArray, \l Int, \l LongLong, \l String, \l
     UInt, or \l ULongLong; otherwise returns 0.0.
 
     \since 4.6
@@ -2704,6 +2706,9 @@ static const quint32 qCanConvertMatrix[QVariant::LastCoreType + 1] =
                 | 1 << QVariant::Char       | 1 << QVariant::ByteArray,
 
 /*double*/        1 << QVariant::Int        | 1 << QVariant::String     | 1 << QVariant::ULongLong
+                | 1 << QVariant::Bool       | 1 << QVariant::UInt       | 1 << QVariant::LongLong
+                | 1 << QVariant::ByteArray,
+/*float*/         1 << QVariant::Int        | 1 << QVariant::String     | 1 << QVariant::ULongLong
                 | 1 << QVariant::Bool       | 1 << QVariant::UInt       | 1 << QVariant::LongLong
                 | 1 << QVariant::ByteArray,
 
@@ -2796,13 +2801,7 @@ static const quint32 qCanConvertMatrix[QVariant::LastCoreType + 1] =
 */
 bool QVariant::canConvert(Type t) const
 {
-    //we can treat floats as double
-    //the reason for not doing it the "proper" way is that QMetaType::Float's value is 135,
-    //which can't be handled by qCanConvertMatrix
-    //In addition QVariant::Type doesn't have a Float value, so we're using QMetaType::Float
-    const uint currentType = ((d.type == QMetaType::Float) ? QVariant::Double : d.type);
-    if (uint(t) == uint(QMetaType::Float)) t = QVariant::Double;
-
+    const uint currentType = d.type;
     if (currentType == uint(t))
         return true;
 
@@ -2961,12 +2960,12 @@ bool QVariant::convert(Type t)
 static bool qIsNumericType(uint tp)
 {
     return (tp >= QVariant::Bool && tp <= QVariant::Double)
-           || (tp >= QMetaType::Long && tp <= QMetaType::Float);
+           || (tp >= QMetaType::Long && tp <= QVariant::Float);
 }
 
 static bool qIsFloatingPoint(uint tp)
 {
-    return tp == QVariant::Double || tp == QMetaType::Float;
+    return tp == QVariant::Double || tp == QVariant::Float;
 }
 
 /*! \internal

src/core/kernel/qvariant.h
@@ -97,6 +97,7 @@ class Q_CORE_EXPORT QVariant
         LongLong = QMetaType::LongLong,
         ULongLong = QMetaType::ULongLong,
         Double = QMetaType::Double,
+        Float = QMetaType::Float,
         Char = QMetaType::QChar,
         Map = QMetaType::QVariantMap,
         List = QMetaType::QVariantList,
@@ -172,7 +173,7 @@ class Q_CORE_EXPORT QVariant
     QVariant(qulonglong ull);
     QVariant(bool b);
     QVariant(double d);
-    QVariant(float f) { d.is_null = false; d.type = QMetaType::Float; d.data.f = f; }
+    QVariant(float f);
 #ifndef QT_NO_CAST_FROM_ASCII
     QT_ASCII_CAST_WARN_CONSTRUCTOR QVariant(const char *str);
 #endif
@@ -236,8 +237,6 @@ class Q_CORE_EXPORT QVariant
     void detach();
     inline bool isDetached() const { return !d.is_shared || d.data.shared->ref == 1; }
 
-
-
     int toInt(bool *ok = Q_NULLPTR) const;
     uint toUInt(bool *ok = Q_NULLPTR) const;
     qlonglong toLongLong(bool *ok = Q_NULLPTR) const;
@@ -288,7 +287,6 @@ class Q_CORE_EXPORT QVariant
 #endif
     static const char *typeToName(Type type);
     static Type nameToType(const char *name);
-
 
     void *data();
     const void *constData() const;

commit 98641e1854b0c046a145b640ac3c7ea0bf54de5d

fix QVariant::Float type conversion regression since 71bd691d09470882be77dfa51f1509362c196d6b
Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

src/core/kernel/qvariant.cpp
@@ -2755,6 +2755,7 @@ bool QVariant::canConvert(Type t) const
     } else if (t == QVariant::Bool) {
         switch (currentType) {
         case QVariant::Double:
+        case QVariant::Float:
         case QVariant::Int:
         case QVariant::UInt:
         case QVariant::LongLong:
@@ -2771,6 +2772,7 @@ bool QVariant::canConvert(Type t) const
         case QVariant::UInt:
         case QVariant::String:
         case QVariant::Double:
+        case QVariant::Float:
         case QVariant::Bool:
         case QVariant::LongLong:
         case QVariant::ULongLong:
@@ -2785,6 +2787,7 @@ bool QVariant::canConvert(Type t) const
         case QVariant::Int:
         case QVariant::String:
         case QVariant::Double:
+        case QVariant::Float:
         case QVariant::Bool:
         case QVariant::LongLong:
         case QVariant::ULongLong:
@@ -2799,6 +2802,7 @@ bool QVariant::canConvert(Type t) const
         case QVariant::Int:
         case QVariant::String:
         case QVariant::Double:
+        case QVariant::Float:
         case QVariant::Bool:
         case QVariant::UInt:
         case QVariant::ULongLong:
@@ -2813,6 +2817,7 @@ bool QVariant::canConvert(Type t) const
         case QVariant::Int:
         case QVariant::String:
         case QVariant::Double:
+        case QVariant::Float:
         case QVariant::Bool:
         case QVariant::UInt:
         case QVariant::LongLong:
@@ -2861,6 +2866,7 @@ bool QVariant::canConvert(Type t) const
         case QVariant::UInt:
         case QVariant::Bool:
         case QVariant::Double:
+        case QVariant::Float:
         case QVariant::Date:
         case QVariant::Time:
         case QVariant::DateTime:
@@ -2887,6 +2893,7 @@ bool QVariant::canConvert(Type t) const
         case QVariant::UInt:
         case QVariant::Bool:
         case QVariant::Double:
+        case QVariant::Float:
         case QVariant::LongLong:
         case QVariant::ULongLong:
             return true;
@@ -3061,6 +3068,7 @@ bool QVariant::canConvert(Type t) const
                 case QVariant::UInt:
                 case QVariant::String:
                 case QVariant::Double:
+                case QVariant::Float:
                 case QVariant::Bool:
                 case QVariant::LongLong:
                 case QVariant::ULongLong: