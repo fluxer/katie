fluxer/katie

commit fb253d03c77d31fd4d56c93d8ee961c8d4ddb0c1

registerFont() function cleanup
Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

src/gui/text/qfontdatabase_x11.h
@@ -1235,7 +1235,6 @@ static void registerFont(QFontDatabasePrivate::ApplicationFont *fnt)
             return;
     }
 
-    QString fileNameForQuery = fnt->fileName;
     int id = 0;
     FcBlanks *blanks = FcConfigGetBlanks(0);
     int count = 0;
@@ -1243,9 +1242,8 @@ static void registerFont(QFontDatabasePrivate::ApplicationFont *fnt)
     QStringList families;
     QFontDatabasePrivate *db = privateDb();
 
-    FcPattern *pattern = Q_NULLPTR;
-    do {
-        pattern = queryFont((const FcChar8 *)QFile::encodeName(fileNameForQuery).constData(),
+    while (id < count) {
+        FcPattern *pattern = queryFont((const FcChar8 *)QFile::encodeName(fnt->fileName).constData(),
                             fnt->data, id, blanks, &count);
         if (!pattern)
             return;
@@ -1254,9 +1252,9 @@ static void registerFont(QFontDatabasePrivate::ApplicationFont *fnt)
         QByteArray cs = fnt->fileName.toUtf8();
         FcPatternAddString(pattern, FC_FILE, (const FcChar8 *) cs.constData());
 
-        FcChar8 *fam = 0, *familylang = 0;
-        int i, n = 0;
-        for (i = 0; ; i++) {
+        int n = 0;
+        for (int i = 0; ; i++) {
+            FcChar8 *familylang = Q_NULLPTR;
             if (FcPatternGetString(pattern, FC_FAMILYLANG, i, &familylang) != FcResultMatch)
                 break;
             QString familyLang = QString::fromUtf8((const char *) familylang);
@@ -1266,16 +1264,16 @@ static void registerFont(QFontDatabasePrivate::ApplicationFont *fnt)
             }
         }
 
+        FcChar8 *fam = Q_NULLPTR;
         if (FcPatternGetString(pattern, FC_FAMILY, n, &fam) == FcResultMatch) {
-            QString family = QString::fromUtf8(reinterpret_cast<const char *>(fam));
-            families << family;
+            families << QString::fromUtf8(reinterpret_cast<const char *>(fam));
         }
 
         if (!FcFontSetAdd(set, pattern))
             return;
 
         ++id;
-    } while (pattern && id < count);
+    }
 
     fnt->families = families;
 #endif

commit bcd0749a0ffd7a2b70b9908b61e6298ca7ebe185

fix registerFont() function regression since fb253d03c77d31fd4d56c93d8ee961c8d4ddb0c1
Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

src/gui/text/qfontdatabase_x11.h
@@ -1242,7 +1242,7 @@ static void registerFont(QFontDatabasePrivate::ApplicationFont *fnt)
     QStringList families;
     QFontDatabasePrivate *db = privateDb();
 
-    while (id < count) {
+    do {
         FcPattern *pattern = queryFont((const FcChar8 *)QFile::encodeName(fnt->fileName).constData(),
                             fnt->data, id, blanks, &count);
         if (!pattern)
@@ -1273,7 +1273,7 @@ static void registerFont(QFontDatabasePrivate::ApplicationFont *fnt)
             return;
 
         ++id;
-    }
+    } while (id < count);
 
     fnt->families = families;
 #endif