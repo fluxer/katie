00176210c4a7123f1413f79ed92fd609fba3a80f
Ivailo Monev <xakepa10@laimg.moc>
2019-12-28 00:12:02 +0000

    remove redundant QT_NO_EXCEPTIONS definition from minsize.sh script [ci skip]
    exceptions are automatically disabled via Q_COMPILER_EXCEPTIONS
    
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- scripts/minsize.sh
+++ scripts/minsize.sh
@@ -14,6 +14,5 @@ cmake ../ -DCMAKE_BUILD_TYPE=MinSizeRel \
     -DCMAKE_INSTALL_PREFIX=/usr \
     -DKATIE_SETTINGS_FULL=/etc/xdg \
     -DKATIE_LDCONF_FULL=/etc/ld.so.conf.d \
-    -DKATIE_PROFILE_FULL=/etc/profile.d \
-    -DQT_NO_EXCEPTIONS=1 $@
+    -DKATIE_PROFILE_FULL=/etc/profile.d $@
 make -j$(nproc || echo 1)
