5b459cf0ec4421c96fc17f4c4fea76f1df7a3c24
Ivailo Monev <xakepa10@laimg.moc>
2019-12-29 23:21:34 +0000

    update copyright and add script for doing so
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

---
+++ scripts/updatecopyright.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import os
+
+licenses = ['BSD', 'FDL', 'LGPL', 'LGPL3', 'LGPL21']
+
+lfiles = []
+for root, subdirs, files in os.walk(os.curdir):
+    for sfile in files:
+        if sfile.endswith(('.cpp', '.h', '.js', '.qs', '.qml', '.ui')):
+            lfiles.append('%s/%s' % (root, sfile))
+
+def readlicense(sfile):
+    sheader = ''
+    with open(sfile, 'r') as f:
+        shouldappend = False
+        for sline in f.readlines():
+            if 'QT_BEGIN_LICENSE' in sline:
+                shouldappend = True
+            elif 'QT_END_LICENSE' in sline:
+                sheader = '%s%s' % (sheader, sline)
+                shouldappend = False
+            if shouldappend:
+                sheader = '%s%s' % (sheader, sline)
+    return sheader
+
+for sfile in lfiles:
+    with open(sfile, 'r') as f:
+        scontent = f.read()
+    for license in licenses:
+        if ('$QT_BEGIN_LICENSE:%s$' % license) in scontent:
+            snewheader = readlicense('%s/header.%s' % (os.curdir, license))
+            soldheader = readlicense(sfile)
+            snewcontent = scontent.replace(soldheader, snewheader)
+            snewcontent = snewcontent.replace("2016-2019", "2016-2020")
+            if not snewcontent == scontent:
+                with open(sfile, 'w') as f:
+                    f.write(snewcontent)
+            
+                
--- tests/auto/qdate/tst_qdate.cpp
+++ tests/auto/qdate/tst_qdate.cpp
@@ -1,19 +1,19 @@
 /****************************************************************************
 **
 ** Copyright (C) 2015 The Qt Company Ltd.
-** Copyright (C) 2016-2019 Ivailo Monev
+** Copyright (C) 2016-2020 Ivailo Monev
 **
 ** This file is part of the test suite of the Katie Toolkit.
 **
 ** $QT_BEGIN_LICENSE:LGPL$
+**
 ** GNU Lesser General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 or version 3 as published by the Free
-** Software Foundation and appearing in the file LICENSE.LGPLv21 and
-** LICENSE.LGPLv3 included in the packaging of this file. Please review the
-** following information to ensure the GNU Lesser General Public License
-** requirements will be met: https://www.gnu.org/licenses/lgpl.html and
-** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+** This file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
 **
 ** As a special exception, The Qt Company gives you certain additional
 ** rights. These rights are described in The Qt Company LGPL Exception
