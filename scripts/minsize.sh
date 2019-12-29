5657c86ed0774a5aa89a62456363c8ef70f98155
Ivailo Monev <xakepa10@laimg.moc>
2019-12-29 09:51:38 +0000

    change KATIE_TRANSLATIONS_FULL, KATIE_LDCONF_FULL and KATIE_PROFILE_FULL defaults to common values [ci skip]
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- scripts/minsize.sh
+++ scripts/minsize.sh
@@ -11,8 +11,5 @@ cd "$cwd/../minsize"
 export CFLAGS="$CFLAGS -flto -fno-exceptions" CXXFLAGS="$CXXFLAGS -flto -fno-exceptions"
 
 cmake ../ -DCMAKE_BUILD_TYPE=MinSizeRel \
-    -DCMAKE_INSTALL_PREFIX=/usr \
-    -DKATIE_SETTINGS_FULL=/etc/xdg \
-    -DKATIE_LDCONF_FULL=/etc/ld.so.conf.d \
-    -DKATIE_PROFILE_FULL=/etc/profile.d $@
+    -DCMAKE_INSTALL_PREFIX=/usr $@
 make -j$(nproc || echo 1)
