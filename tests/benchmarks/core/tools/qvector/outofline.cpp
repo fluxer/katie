44fbf82b276057fbefe6d1082490380ecd749bb2
Ivailo Monev <xakepa10@laimg.moc>
2020-01-19 22:09:44 +0000

    remove QRawVector<T> from QVector<T> benchmark
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- tests/benchmarks/core/tools/qvector/outofline.cpp
+++ tests/benchmarks/core/tools/qvector/outofline.cpp
@@ -32,8 +32,6 @@
 ****************************************************************************/
 
 #include <QVector>
-#include <vector>
-#include "qrawvector.h"
 
 const int N = 1000000;
 double s = 0;
@@ -44,14 +42,6 @@ QVector<double> qvector_fill_and_return_helper()
     for (int i = 0; i != N; ++i)
         v[i] = i;
     return v;
-}
-
-QVector<double> qrawvector_fill_and_return_helper()
-{
-    QRawVector<double> v(N);
-    for (int i = 0; i != N; ++i)
-        v[i] = i;
-    return v.mutateToVector();
 }
 
 QVector<double> mixedvector_fill_and_return_helper()
