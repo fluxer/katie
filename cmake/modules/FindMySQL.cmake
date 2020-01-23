1102ecc943869c9dfba129f1f1a721f82da2b30f
Ivailo Monev <xakepa10@laimg.moc>
2020-01-23 15:19:25 +0000

    require MySQL/MariaDB atleast v4.1.13
    also check for MariaDB specific config program, header in path suffix and
    library name. setting MYSQL_VERSION based on PC_MYSQL_VERSION is no longer
    done since it does not report its actual version, e.g. reports 3.1.4 while
    it is 10.3.18 (on Debian stable) for an example
    
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- cmake/modules/FindMySQL.cmake
+++ cmake/modules/FindMySQL.cmake
@@ -19,50 +19,46 @@ if(NOT WIN32)
     pkg_check_modules(PC_MYSQL QUIET libmariadb)
 endif()
 
-# However, both provide mysql_config
-if(NOT PC_MYSQL_FOUND)
-    find_program(MYSQL_CONFIG
-        NAMES
-        mysql_config
-        HINTS
-        $ENV{MYSQLDIR}/bin
-    )
+# However, both provide config program
+find_program(MYSQL_CONFIG
+    NAMES
+    mariadb_config mysql_config
+    HINTS
+    $ENV{MYSQLDIR}/bin
+)
 
-    if(MYSQL_CONFIG)
-        execute_process(
-            COMMAND ${MYSQL_CONFIG} --variable=pkgincludedir
-            RESULT_VARIABLE procerror1
-            OUTPUT_VARIABLE MYSQL_INCLUDES
-            OUTPUT_STRIP_TRAILING_WHITESPACE
-        )
-        execute_process(
-            COMMAND ${MYSQL_CONFIG} --libmysqld-libs
-            RESULT_VARIABLE proceerror2
-            OUTPUT_VARIABLE MYSQL_LIBRARIES
-            OUTPUT_STRIP_TRAILING_WHITESPACE
-        )
-        execute_process(
-            COMMAND ${MYSQL_CONFIG} --version
-            RESULT_VARIABLE proceerror3
-            OUTPUT_VARIABLE MYSQL_VERSION
-            OUTPUT_STRIP_TRAILING_WHITESPACE
-        )
-        # just in case, MYSQL_VERSION is not critical
-        if(NOT procerror1 STREQUAL "0" OR NOT proceerror2 STREQUAL "0")
-            set(MYSQL_INCLUDES)
-            set(MYSQL_LIBRARIES)
-            set(MYSQL_VERSION)
-        endif()
+if(MYSQL_CONFIG)
+    execute_process(
+        COMMAND ${MYSQL_CONFIG} --variable=pkgincludedir
+        RESULT_VARIABLE procerror1
+        OUTPUT_VARIABLE MYSQL_INCLUDES
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    execute_process(
+        COMMAND ${MYSQL_CONFIG} --libmysqld-libs
+        RESULT_VARIABLE proceerror2
+        OUTPUT_VARIABLE MYSQL_LIBRARIES
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    execute_process(
+        COMMAND ${MYSQL_CONFIG} --version
+        RESULT_VARIABLE proceerror3
+        OUTPUT_VARIABLE MYSQL_VERSION
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    # just in case, MYSQL_VERSION is not critical
+    if(NOT procerror1 STREQUAL "0" OR NOT proceerror2 STREQUAL "0")
+        set(MYSQL_INCLUDES)
+        set(MYSQL_LIBRARIES)
+        set(MYSQL_VERSION)
     endif()
-else()
-    set(MYSQL_VERSION "${PC_MYSQL_VERSION}")
 endif()
 
 if(NOT MYSQL_INCLUDES OR NOT MYSQL_LIBRARIES)
     find_path(MYSQL_INCLUDES
         NAMES
         mysql.h
-        PATH_SUFFIXES mysql
+        PATH_SUFFIXES mariadb mysql
         HINTS
         $ENV{MYSQLDIR}/include
         ${PC_MYSQL_INCLUDEDIR}
@@ -70,7 +66,7 @@ if(NOT MYSQL_INCLUDES OR NOT MYSQL_LIBRARIES)
     )
 
     find_library(MYSQL_LIBRARIES
-        mysqld
+        mariadbd mysqld
         HINTS
         $ENV{MYSQLDIR}/lib
         ${PC_MYSQL_LIBDIR}
