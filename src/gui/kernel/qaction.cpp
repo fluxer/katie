fluxer/katie

commit d504e5d4adbcd424377c526c2d35a8479e51d82d

QAction cleanups
Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

src/gui/kernel/qaction.cpp
@@ -79,8 +79,8 @@ static QString qt_strippedText(QString s)
 }
 
 
-QActionPrivate::QActionPrivate() : group(0), enabled(1), forceDisabled(0),
-                                   visible(1), forceInvisible(0), checkable(0), checked(0), separator(0), fontSet(false),
+QActionPrivate::QActionPrivate() : group(0), enabled(1),
+                                   visible(1), checkable(0), checked(0), separator(0), fontSet(false),
                                    menuRole(QAction::TextHeuristicRole),
                                    priority(QAction::NormalPriority)
 {
@@ -544,7 +544,7 @@ bool QAction::autoRepeat() const
     \brief the action's font
 
     The font property is used to render the text set on the
-    QAction. The font will can be considered a hint as it will not be
+    QAction. The font can be considered a hint as it will not be
     consulted in all cases based upon application and style.
 
     By default, this property contains the application's default font.
@@ -1030,9 +1030,8 @@ bool QAction::isChecked() const
 void QAction::setEnabled(bool b)
 {
     Q_D(QAction);
-    if (b == d->enabled && b != d->forceDisabled)
+    if (b == d->enabled)
         return;
-    d->forceDisabled = !b;
     if (b && (!d->visible || (d->group && !d->group->isEnabled())))
         return;
     QAPP_CHECK("setEnabled");
@@ -1065,12 +1064,11 @@ bool QAction::isEnabled() const
 void QAction::setVisible(bool b)
 {
     Q_D(QAction);
-    if (b == d->visible && b != d->forceInvisible)
+    if (b == d->visible)
         return;
     QAPP_CHECK("setVisible");
-    d->forceInvisible = !b;
     d->visible = b;
-    d->enabled = b && !d->forceDisabled && (!d->group || d->group->isEnabled()) ;
+    d->enabled = b && d->enabled && (!d->group || d->group->isEnabled()) ;
 #ifndef QT_NO_SHORTCUT
     d->setShortcutEnabled(d->enabled, qApp->d_func()->shortcutMap);
 #endif
@@ -1146,7 +1144,8 @@ QAction::setData(const QVariant &data)
 bool
 QAction::showStatusText(QWidget *widget)
 {
-    return d_func()->showStatusText(widget, statusTip());
+    Q_D(QAction);
+    return d->showStatusText(widget, d->statustip);
 }
 
 /*!

src/gui/kernel/qactiongroup.cpp
@@ -182,14 +182,8 @@ QAction *QActionGroup::addAction(QAction* a)
         QObject::connect(a, SIGNAL(changed()), this, SLOT(_q_actionChanged()));
         QObject::connect(a, SIGNAL(hovered()), this, SLOT(_q_actionHovered()));
     }
-    if(!a->d_func()->forceDisabled) {
-        a->setEnabled(d->enabled);
-        a->d_func()->forceDisabled = false;
-    }
-    if(!a->d_func()->forceInvisible) {
-        a->setVisible(d->visible);
-        a->d_func()->forceInvisible = false;
-    }
+    a->setEnabled(d->enabled);
+    a->setVisible(d->visible);
     if(a->isChecked())
         d->current = a;
     QActionGroup *oldGroup = a->d_func()->group;
@@ -302,10 +296,7 @@ void QActionGroup::setEnabled(bool b)
     Q_D(QActionGroup);
     d->enabled = b;
     for(QList<QAction*>::const_iterator it = d->actions.constBegin(); it != d->actions.constEnd(); ++it) {
-        if(!(*it)->d_func()->forceDisabled) {
-            (*it)->setEnabled(b);
-            (*it)->d_func()->forceDisabled = false;
-        }
+        (*it)->setEnabled(b);
     }
 }
 
@@ -339,10 +330,7 @@ void QActionGroup::setVisible(bool b)
     Q_D(QActionGroup);
     d->visible = b;
     for(QList<QAction*>::Iterator it = d->actions.begin(); it != d->actions.end(); ++it) {
-        if(!(*it)->d_func()->forceInvisible) {
-            (*it)->setVisible(b);
-            (*it)->d_func()->forceInvisible = false;
-        }
+        (*it)->setVisible(b);
     }
 }
 

src/gui/kernel/qaction.h
@@ -155,9 +155,7 @@ public:
     void setData(const QVariant &var);
 
     bool isChecked() const;
-
     bool isEnabled() const;
-
     bool isVisible() const;
 
     enum ActionEvent { Trigger, Hover };

src/gui/kernel/qaction_p.h
@@ -72,11 +72,6 @@ public:
     QActionPrivate();
     ~QActionPrivate();
 
-    static QActionPrivate *get(QAction *q)
-    {
-        return q->d_func();
-    }
-
     bool showStatusText(QWidget *w, const QString &str);
 
     QPointer<QActionGroup> group;
@@ -97,15 +92,15 @@ public:
     Qt::ShortcutContext shortcutContext;
     uint autorepeat : 1;
 #endif
-    QFont font;
-    QPointer<QMenu> menu;
-    uint enabled : 1, forceDisabled : 1;
-    uint visible : 1, forceInvisible : 1;
+    uint enabled : 1;
+    uint visible : 1;
     uint checkable : 1;
     uint checked : 1;
     uint separator : 1;
     uint fontSet : 1;
 
+    QFont font;
+    QPointer<QMenu> menu;
     QAction::MenuRole menuRole;
     QAction::Priority priority;
 
@@ -117,8 +112,6 @@ public:
     void redoGrab(QShortcutMap &map);
     void redoGrabAlternate(QShortcutMap &map);
     void setShortcutEnabled(bool enable, QShortcutMap &map);
-
-    static QShortcutMap *globalMap;
 #endif // QT_NO_SHORTCUT
 
     void sendDataChanged();

commit 7de3b1c8f1a3764205cdebcdba8f21d00d30b673

fix QAction::setVisible() regression since d504e5d4adbcd424377c526c2d35a8479e51d82d
also update the documentation about group state change that was made with
d504e5d4adbcd424377c526c2d35a8479e51d82d

src/gui/kernel/qaction.cpp
@@ -1059,7 +1059,7 @@ void QAction::setVisible(bool b)
         return;
     QAPP_CHECK("setVisible");
     d->visible = b;
-    d->enabled = b && d->enabled && (!d->group || d->group->isEnabled()) ;
+    d->enabled = b && (!d->group || d->group->isEnabled()) ;
 #ifndef QT_NO_SHORTCUT
     d->setShortcutEnabled(d->enabled, qApp->d_func()->shortcutMap);
 #endif

src/gui/kernel/qactiongroup.cpp
@@ -278,8 +278,7 @@ bool QActionGroup::isExclusive() const
     \property QActionGroup::enabled
     \brief whether the action group is enabled
 
-    Each action in the group will be enabled or disabled unless it
-    has been explicitly disabled.
+    Each action in the group will be enabled or disabled.
 
     \sa QAction::setEnabled()
 */
@@ -313,7 +312,7 @@ QAction *QActionGroup::checkedAction() const
     \brief whether the action group is visible
 
     Each action in the action group will match the visible state of
-    this group unless it has been explicitly hidden.
+    this group.
 
     \sa QAction::setEnabled()
 */