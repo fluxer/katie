07bf0927105c0ca2191322a474385726dd8aaa56
Ivailo Monev <xakepa10@laimg.moc>
2020-03-08 23:17:18 +0000

    always use full paths for installation
    now that CPack is no longer supported relative paths are no longer
    required. this change also fixes the headers optimizations upon
    installation since it requires full paths to work
    
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- src/tools/designer/designer.desktop.cmake
+++ src/tools/designer/designer.desktop.cmake
@@ -1,8 +1,8 @@
 [Desktop Entry]
 Name=Katie Designer
 Comment=Design GUIs for Katie applications
-Exec=@KATIE_BINARIES_FULL@/designer@KATIE_TOOLS_SUFFIX@@CMAKE_EXECUTABLE_SUFFIX@ %F
-Icon=@KATIE_PIXMAPS_FULL@/designer@KATIE_TOOLS_SUFFIX@.png
+Exec=@KATIE_BINARIES_PATH@/designer@KATIE_TOOLS_SUFFIX@@CMAKE_EXECUTABLE_SUFFIX@ %F
+Icon=@KATIE_PIXMAPS_PATH@/designer@KATIE_TOOLS_SUFFIX@.png
 MimeType=application/x-designer;
 Terminal=false
 Encoding=UTF-8
