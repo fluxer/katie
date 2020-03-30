fluxer/katie

commit 91019ff8de9ba23a45c7920184e473620e5244d0

validate stack size in QThread::setStackSize()
that way threads will not fail if it is less than the minimum, the warning
from QThread::start() is basically invisible to GUI users unless
application is started from terminal which is far from ideal. one has
to install own message handler via qInstallMsgHandler() to make such
warnings noticable to GUI users, possible via QErrorMessage, which should
be errors in fact and printed via qCritical() but making such changes is
beyond the scope of this commit

Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

src/core/thread/qthread.cpp
@@ -42,6 +42,8 @@
 #include "qcoreapplication_p.h"
 #include "qscopedpointer.h"
 
+#include <limits.h> // for PTHREAD_STACK_MIN
+
 QT_BEGIN_NAMESPACE
 
 /*
@@ -432,6 +434,15 @@ void QThread::setStackSize(uint stackSize)
     QMutexLocker locker(&d->mutex);
     Q_ASSERT_X(!d->running, "QThread::setStackSize",
                "cannot change stack size while the thread is running");
+#ifdef PTHREAD_STACK_MIN
+    int stack_min = sysconf(_SC_THREAD_STACK_MIN);
+    if (stack_min == -1)
+        stack_min = PTHREAD_STACK_MIN;
+    if (Q_UNLIKELY(stackSize < stack_min)) {
+        qWarning("QThread::setStackSize: %u is less than the minimum %u", stackSize, stack_min);
+        stackSize = stack_min;
+    }
+#endif
     d->stackSize = stackSize;
 }
 

commit 63558532694087665c4aee914725a5433ff68521

fix tst_QThread::setStackSize() test case
also fixes a regression since 91019ff8de9ba23a45c7920184e473620e5244d0

Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

src/core/thread/qthread.cpp
@@ -438,7 +438,8 @@ void QThread::setStackSize(uint stackSize)
     static int stack_min = sysconf(_SC_THREAD_STACK_MIN);
     if (stack_min == -1)
         stack_min = PTHREAD_STACK_MIN;
-    if (Q_UNLIKELY(stackSize < stack_min)) {
+    // 0 means default stack size
+    if (Q_UNLIKELY(stackSize != 0 && stackSize < stack_min)) {
         qWarning("QThread::setStackSize: %u is less than the minimum %u", stackSize, stack_min);
         stackSize = stack_min;
     }

tests/auto/qthread/tst_qthread.cpp
@@ -407,10 +407,16 @@ void tst_QThread::priority()
 
 void tst_QThread::setStackSize()
 {
+#ifdef PTHREAD_STACK_MIN
+    static const uint stack_size = PTHREAD_STACK_MIN * 2;
+#else
+    static const uint stack_size = 16384; // default on Linux
+#endif
+
     Simple_Thread thread;
     QCOMPARE(thread.stackSize(), 0u);
-    thread.setStackSize(8192u);
-    QCOMPARE(thread.stackSize(), 8192u);
+    thread.setStackSize(stack_size);
+    QCOMPARE(thread.stackSize(), stack_size);
     thread.setStackSize(0u);
     QCOMPARE(thread.stackSize(), 0u);
 }