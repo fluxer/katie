238dafaf6d9330c0aa7581b1e699e050d45af6db
Ivailo Monev <xakepa10@laimg.moc>
2020-06-22 16:44:21 +0000

    remove redundant Q_UNUSED() macro calls in QCompletionEngine::filterHistory()
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- src/gui/util/qcompleter.cpp
+++ src/gui/util/qcompleter.cpp
@@ -465,8 +465,6 @@ QMatchData QCompletionEngine::filterHistory()
         return QMatchData();
     bool isDirModel = false;
     bool isFsModel = false;
-    Q_UNUSED(isDirModel)
-    Q_UNUSED(isFsModel)
 #ifndef QT_NO_DIRMODEL
     isDirModel = (qobject_cast<QDirModel *>(source) != Q_NULLPTR);
 #endif
