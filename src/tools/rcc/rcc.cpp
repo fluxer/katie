a45ee728546582585a5f64800e3c7a4ea9b63db8
Ivailo Monev <xakepa10@gmail.com>
2020-09-12 16:59:07 +0300

    mark digits variable in RCCResourceLibrary::writeHex() as static
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/tools/rcc/rcc.cpp
+++ src/tools/rcc/rcc.cpp
@@ -683,7 +683,7 @@ bool RCCResourceLibrary::output(QIODevice &outDevice, QIODevice &errorDevice)
 
 void RCCResourceLibrary::writeHex(quint8 tmp)
 {
-    const char * const digits = "0123456789abcdef";
+    static const char * const digits = "0123456789abcdef";
     writeChar('0');
     writeChar('x');
     if (tmp < 16) {
