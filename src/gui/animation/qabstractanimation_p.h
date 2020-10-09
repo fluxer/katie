88184c47bd664eed71998fed6cb5265b61049a49
Ivailo Monev <xakepa10@gmail.com>
2020-10-09 14:02:36 +0300

    remove redundant QUnifiedTimer::instance() argument
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/gui/animation/qabstractanimation_p.h
+++ src/gui/animation/qabstractanimation_p.h
@@ -132,7 +132,7 @@ private:
 
 public:
     //XXX this is needed by dui
-    static QUnifiedTimer *instance(bool create = true);
+    static QUnifiedTimer *instance();
 
     static void registerAnimation(QAbstractAnimation *animation, bool isTopLevel);
     static void unregisterAnimation(QAbstractAnimation *animation);
