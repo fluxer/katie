04f45cd53a751899a72ed0373e6f6db5492b67c2
Ivailo Monev <xakepa10@gmail.com>
2020-10-21 00:40:40 +0300

    check for sockaddr_ll and sockaddr_dl during build
    apparently illumos has sockaddr_ll much like Linux
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/network/kernel/qnetworkinterface_unix.cpp
+++ src/network/kernel/qnetworkinterface_unix.cpp
@@ -282,7 +282,7 @@ QT_BEGIN_INCLUDE_NAMESPACE
 QT_END_INCLUDE_NAMESPACE
 # endif
 
-# if defined(Q_OS_LINUX) && defined(__GLIBC__)
+# if defined(QT_HAVE_SOCKADDR_LL)
 #  include <netpacket/packet.h>
 
 static QList<QNetworkInterfacePrivate *> createInterfaces(ifaddrs *rawList)
@@ -326,7 +326,7 @@ static QList<QNetworkInterfacePrivate *> createInterfaces(ifaddrs *rawList)
     return interfaces;
 }
 
-# elif defined(Q_OS_BSD4)
+# elif defined(QT_HAVE_SOCKADDR_DL)
 QT_BEGIN_INCLUDE_NAMESPACE
 #  include <net/if_dl.h>
 QT_END_INCLUDE_NAMESPACE
