40b140a2dc8e50d78ec96e9303436c0f7a12b623
Ivailo Monev <xakepa10@gmail.com>
2020-10-24 23:13:56 +0300

    optimize QWidgetPrivate::mapToWS() and QWidgetPrivate::mapFromWS()
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/gui/kernel/qwidget_p.h
+++ src/gui/kernel/qwidget_p.h
@@ -516,10 +516,10 @@ public:
     { return p + data.wrect.topLeft(); }
 
     inline QRect mapToWS(const QRect &r) const
-    { QRect rr(r); rr.translate(-data.wrect.topLeft()); return rr; }
+    { return r.translated(-data.wrect.topLeft()); }
 
     inline QRect mapFromWS(const QRect &r) const
-    { QRect rr(r); rr.translate(data.wrect.topLeft()); return rr; }
+    { return r.translated(data.wrect.topLeft()); }
 
     // Variables.
     // Regular pointers (keep them together to avoid gaps on 64 bit architectures).
