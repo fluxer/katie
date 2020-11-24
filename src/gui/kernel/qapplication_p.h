6ac84bcf372116bb6ed9609eae8c9972bf22bfa4
Ivailo Monev <xakepa10@gmail.com>
2020-11-24 13:31:08 +0000

    remove unused QApplicationPrivate::widgetCount member
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/gui/kernel/qapplication_p.h
+++ src/gui/kernel/qapplication_p.h
@@ -189,7 +189,6 @@ public:
     static bool fade_menu;
     static bool fade_tooltip;
     static bool animate_toolbox;
-    static bool widgetCount; // Coupled with -widgetcount switch
 
     static void setSystemPalette(const QPalette &pal);
     static void setPalette_helper(const QPalette &palette, const char* className, bool clearWidgetPaletteHash);
