c8ab56a429d8741cbcc6d190f0ed87ef904e61e2
Ivailo Monev <xakepa10@gmail.com>
2020-12-02 05:43:11 +0000

    execute D-Bus tests with script
    NetBSD does not have the daemon running by default and some environments
    such as Travis may not have it either, this should make the tests pass
    in such cases without any changes to the host
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

---
+++ cmake/dbus.sh.cmake
@@ -0,0 +1,17 @@
+#!@KATIE_SHELL@
+
+set -e
+
+if [ -z "$LD_LIBRARY_PATH" ];then
+    export LD_LIBRARY_PATH="@CMAKE_BINARY_DIR@/lib"
+else
+    export LD_LIBRARY_PATH="@CMAKE_BINARY_DIR@/lib:$LD_LIBRARY_PATH"
+fi
+export QT_PLUGIN_PATH="@CMAKE_BINARY_DIR@/plugins"
+export QML_IMPORT_PATH="@CMAKE_BINARY_DIR@/imports"
+
+bin="$1"
+shift
+
+cd "$(dirname "$bin")"
+exec dbus-launch @KATIE_CROSS_EXEC@ "./$(basename "$bin")" $@
