fluxer/katie

commit e29752f0f6bf9884ed8644e375143f05b7c3bd21

remove redundant pre-processor checks
SIOCGIFNAME and SIOCGIFHWADDR are not documented as appearing in certain
versions of standards or Linux versions thus I assume they should be
defined

Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

src/network/kernel/qnetworkinterface_unix.cpp
@@ -39,8 +39,6 @@
 
 #ifndef QT_NO_NETWORKINTERFACE
 
-#define IP_MULTICAST    // make AIX happy and define IFF_MULTICAST
-
 #include <sys/types.h>
 #include <sys/socket.h>
 
@@ -98,13 +96,8 @@ static QNetworkInterface::InterfaceFlags convertFlags(uint rawFlags)
     flags |= (rawFlags & IFF_RUNNING) ? QNetworkInterface::IsRunning : QNetworkInterface::InterfaceFlag(0);
     flags |= (rawFlags & IFF_BROADCAST) ? QNetworkInterface::CanBroadcast : QNetworkInterface::InterfaceFlag(0);
     flags |= (rawFlags & IFF_LOOPBACK) ? QNetworkInterface::IsLoopBack : QNetworkInterface::InterfaceFlag(0);
-#ifdef IFF_POINTOPOINT //cygwin doesn't define IFF_POINTOPOINT
     flags |= (rawFlags & IFF_POINTOPOINT) ? QNetworkInterface::IsPointToPoint : QNetworkInterface::InterfaceFlag(0);
-#endif
-
-#ifdef IFF_MULTICAST
     flags |= (rawFlags & IFF_MULTICAST) ? QNetworkInterface::CanMulticast : QNetworkInterface::InterfaceFlag(0);
-#endif
     return flags;
 }
 
@@ -195,7 +188,6 @@ static QNetworkInterfacePrivate *findInterface(int socket, QList<QNetworkInterfa
     iface->index = ifindex;
     interfaces << iface;
 
-#ifdef SIOCGIFNAME
     // Get the canonical name
     QByteArray oldName = req.ifr_name;
     if (qt_safe_ioctl(socket, SIOCGIFNAME, &req) >= 0) {
@@ -204,7 +196,6 @@ static QNetworkInterfacePrivate *findInterface(int socket, QList<QNetworkInterfa
         // reset the name:
         memcpy(req.ifr_name, oldName, qMin<int>(oldName.length() + 1, sizeof(req.ifr_name) - 1));
     } else
-#endif
     {
         // use this name anyways
         iface->name = QString::fromLatin1(req.ifr_name);
@@ -215,13 +206,11 @@ static QNetworkInterfacePrivate *findInterface(int socket, QList<QNetworkInterfa
         iface->flags = convertFlags(req.ifr_flags);
     }
 
-#ifdef SIOCGIFHWADDR
     // Get the HW address
     if (qt_safe_ioctl(socket, SIOCGIFHWADDR, &req) >= 0) {
         uchar *addr = (uchar *)req.ifr_addr.sa_data;
         iface->hardwareAddress = iface->makeHwAddress(6, addr);
     }
-#endif
 
     return iface;
 }
@@ -230,8 +219,8 @@ static QList<QNetworkInterfacePrivate *> interfaceListing()
 {
     QList<QNetworkInterfacePrivate *> interfaces;
 
-    int socket;
-    if ((socket = qt_safe_socket(AF_INET, SOCK_STREAM, IPPROTO_IP)) == -1)
+    int socket = qt_safe_socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    if (socket == -1)
         return interfaces;      // error
 
     QSet<QByteArray> names = interfaceNames(socket);
@@ -390,12 +379,12 @@ static QList<QNetworkInterfacePrivate *> interfaceListing()
 {
     QList<QNetworkInterfacePrivate *> interfaces;
 
-    int socket;
-    if ((socket = qt_safe_socket(AF_INET, SOCK_STREAM, IPPROTO_IP)) == -1)
+    int socket = qt_safe_socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    if (socket == -1)
         return interfaces;      // error
 
     ifaddrs *interfaceListing;
-    if (getifaddrs(&interfaceListing) == -1) {
+    if (::getifaddrs(&interfaceListing) == -1) {
         // error
         ::close(socket);
         return interfaces;

src/gui/kernel/qkeymapper_x11.cpp
@@ -543,7 +543,6 @@ static const struct KeyTblData {
     { XK_Zen_Koho,                Qt::Key_MultipleCandidate },
     { XK_Mae_Koho,                Qt::Key_PreviousCandidate },
 
-#ifdef XK_KOREAN
     // Korean keyboard support
     { XK_Hangul,                  Qt::Key_Hangul },
     { XK_Hangul_Start,            Qt::Key_Hangul_Start },
@@ -566,7 +565,6 @@ static const struct KeyTblData {
     { XK_Hangul_Special,          Qt::Key_Hangul_Special },
     // { XK_Hangul_switch,         Qt::Key_Hangul_switch },
     { XK_Hangul_switch,           Qt::Key_Mode_switch },
-#endif  // XK_KOREAN
 
     // dead keys
     { XK_dead_grave,              Qt::Key_Dead_Grave },

src/gui/painting/qpainterpath.cpp
@@ -33,7 +33,6 @@
 
 #include "qpainterpath.h"
 #include "qpainterpath_p.h"
-
 #include "qbitmap.h"
 #include "qdebug.h"
 #include "qiodevice.h"
@@ -44,7 +43,6 @@
 #include "qtextlayout.h"
 #include "qvarlengtharray.h"
 #include "qmath.h"
-
 #include "qbezier_p.h"
 #include "qfontengine_p.h"
 #include "qnumeric.h"
@@ -54,14 +52,6 @@
 #include "qtextengine_p.h"
 
 #include <limits.h>
-
-#if 0
-#include <performance.h>
-#else
-#define PM_INIT
-#define PM_MEASURE(x)
-#define PM_DISPLAY
-#endif
 
 QT_BEGIN_NAMESPACE
 

commit 240bca256d91ff5a2eea1f20f579401db305f082

Solaris build fix
regressed with e29752f0f6bf9884ed8644e375143f05b7c3bd21

Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

src/network/kernel/qnetworkinterface_unix.cpp
@@ -185,6 +185,7 @@ static QNetworkInterfacePrivate *findInterface(int socket, QList<QNetworkInterfa
     iface->index = ifindex;
     interfaces << iface;
 
+#ifdef SIOCGIFNAME // not defined on Solaris
     // Get the canonical name
     QByteArray oldName = req.ifr_name;
     if (::ioctl(socket, SIOCGIFNAME, &req) >= 0) {
@@ -193,6 +194,7 @@ static QNetworkInterfacePrivate *findInterface(int socket, QList<QNetworkInterfa
         // reset the name:
         ::memcpy(req.ifr_name, oldName, qMin<int>(oldName.length() + 1, sizeof(req.ifr_name) - 1));
     } else
+#endif
     {
         // use this name anyways
         iface->name = QString::fromLatin1(req.ifr_name);