95d0453b9efc338f37c8abd82d4f760855d404ae
Ivailo Monev <xakepa10@gmail.com>
2020-12-24 15:35:39 +0000

    compiler warning fix
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/core/thread/qthread.cpp
+++ src/core/thread/qthread.cpp
@@ -429,7 +429,7 @@ void QThread::setStackSize(uint stackSize)
         stack_min = PTHREAD_STACK_MIN;
     // 0 means default stack size
     if (Q_UNLIKELY(stackSize != 0 && stackSize < stack_min)) {
-        qWarning("QThread::setStackSize: %u is less than the minimum %u", stackSize, stack_min);
+        qWarning("QThread::setStackSize: %u is less than the minimum %ld", stackSize, stack_min);
         stackSize = stack_min;
     }
 #endif
