53b7276e602c2dc1b4a8591827428e08f1657804
Ivailo Monev <xakepa10@gmail.com>
2020-12-31 19:08:04 +0200

    get rid of internal QT_SCRIPT_LIB and QT_UITOOLS_LIB definitions
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/designer/shared/scriptdialog.cpp
+++ src/designer/shared/scriptdialog.cpp
@@ -41,9 +41,7 @@
 #include <QtGui/QHBoxLayout>
 #include <QtGui/QDialogButtonBox>
 #include <QtGui/QMessageBox>
-#ifdef QT_SCRIPT_LIB
 #include <QtScript/QScriptEngine>
-#endif
 
 QT_BEGIN_NAMESPACE
 
@@ -106,19 +104,15 @@ variables <i>widget</i> and <i>childWidgets</i>, respectively.");
         const QString script = trimmedScript();
         if (script.isEmpty())
             return true;
-#ifdef QT_SCRIPT_LIB
         QScriptEngine scriptEngine;
         if (scriptEngine.canEvaluate(script))
             return true;
         m_dialogGui->message(this, QDesignerDialogGuiInterface::ScriptDialogMessage, QMessageBox::Warning,
                              windowTitle(), tr("Syntax error"), QMessageBox::Ok);
         return  false;
-#else
-        Q_UNUSED(m_dialogGui);
-        return true;
-#endif
     }
 } // namespace qdesigner_internal
 
 QT_END_NAMESPACE
+
 #include "moc_scriptdialog_p.h"
