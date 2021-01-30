cdfd7dc3d618f15f54bf4e43aa64ea2feebb9bd7
Ivailo Monev <xakepa10@gmail.com>
2021-01-30 06:38:19 +0200

    compiles with GCC 8.3.0 and Clang 11.1.0, where is it defined tho?
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/core/io/qbuffer.cpp
+++ src/core/io/qbuffer.cpp
@@ -349,7 +349,7 @@ qint64 QBuffer::size() const
 /*!
     \reimp
 */
-bool QBuffer::seek(qint64 pos)
+bool QBuffer::seek(const qint64 pos)
 {
     Q_D(QBuffer);
     if (pos > d->buf->size() && isWritable()) {
