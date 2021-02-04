5b9d1090d3a0ed8afc2776cb5ec7333304955274
Ivailo Monev <xakepa10@gmail.com>
2021-02-04 12:44:03 +0200

    do not set compiler standard from mkspecs
    for reference:
    https://gcc.gnu.org/onlinedocs/gcc-6.5.0/gcc/Standards.html#C_002b_002b-Language
    https://releases.llvm.org/6.0.0/tools/clang/docs/ReleaseNotes.html
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- mkspecs/mkspecs.cmake
+++ mkspecs/mkspecs.cmake
@@ -16,23 +16,3 @@ endif()
 if(NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
     katie_definition(-DQT_NO_DEBUG)
 endif()
-
-# Set compiler standard to C++ 11, appending to CMAKE_CXX_FLAGS is done so that try_compile() is
-# also affected, which KDE uses for visibility check for an example
-if(NOT CMAKE_VERSION VERSION_LESS "3.8.0")
-    cmake_policy(GET CMP0067 trycompilestandardpolicy)
-else()
-    set(trycompilestandardpolicy OLD)
-endif()
-if(NOT trycompilestandardpolicy EQUAL NEW)
-    # using MATCHES in all checks due to CMP0054
-    if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++11")
-    elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-    else()
-        message(FATAL_ERROR "Unknown compiler '${CMAKE_CXX_COMPILER_ID}'")
-    endif()
-endif()
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_STANDARD 11)
