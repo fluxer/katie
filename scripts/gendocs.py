6f66d978ec2098f1c88ff79f9508ed35ad26353f
Ivailo Monev <xakepa10@gmail.com>
2021-02-15 11:58:46 +0200

    add script to generate documentation
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

---
+++ scripts/gendocs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import sys, os, shutil, subprocess
+
+cwd = os.path.dirname(__file__)
+
+doxygen = None
+for path in os.environ.get('PATH', '/bin:/usr/bin').split(':'):
+    sfile = '%s/doxygen' % path
+    if os.access(sfile, os.X_OK):
+        doxygen = sfile
+        break
+
+if not doxygen:
+    sys.stderr.write('Doxygen is not installed\n')
+    sys.exit(1)
+
+sys.stdout.write('Generating API docs for Katie...\n')
+apidir = '%s/../apidocs' % cwd
+if os.path.isdir(apidir):
+    shutil.rmtree(apidir)
+subprocess.check_call((doxygen, '%s/../doxygen.conf' % cwd))
+
