e1a0105edf9ae8677f4f72cd3afcddbad34e9759
Ivailo Monev <xakepa10@gmail.com>
2021-06-14 15:30:15 +0300

    fix QDomNamedNodeMap::item crash with negative index
    upstream commit:
    https://github.com/qt/qtbase/commit/b20d6cded7be8b86bed93ee705420bfb01700c5b
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/xml/dom/qdom.cpp
+++ src/xml/dom/qdom.cpp
@@ -3114,7 +3114,7 @@ QDomNodePrivate* QDomNamedNodeMapPrivate::removeNamedItem(const QString& name)
 
 QDomNodePrivate* QDomNamedNodeMapPrivate::item(int index) const
 {
-    if (index >= length())
+    if (index >= length() || index < 0)
         return 0;
     return *(map.constBegin() + index);
 }
