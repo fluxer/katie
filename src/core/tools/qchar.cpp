3bf4d107cba905476187f8e9d402f18e08f41eea
Ivailo Monev <xakepa10@gmail.com>
2021-07-11 03:57:08 +0300

    document QChar::UnicodeVersion::Unicode_14_0 enum [ci skip]
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/core/tools/qchar.cpp
+++ src/core/tools/qchar.cpp
@@ -252,6 +252,7 @@ static inline uint to_ascii_upper(uint ucs4)
     \value Unicode_12_0
     \value Unicode_12_1
     \value Unicode_13_0
+    \value Unicode_14_0
     \value Unicode_Last Latest supported version
     \value Unicode_Unassigned  The value is not assigned to any character
         in Unicode.
