3117e6cd3f392fc5964fcfc69833d87d28db780d
Ivailo Monev <xakepa10@gmail.com>
2021-09-08 02:38:50 +0300

    stripdiff rewrite [ci skip]
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- scripts/stripdiff.sh
+++ scripts/stripdiff.sh
@@ -5,14 +5,20 @@ set -e
 sofile="$@"
 sobase="$(basename $sofile)"
 sosym="/tmp/$sobase.sym"
+usosym="/tmp/uniq_$sobase.sym"
 stripfile="/tmp/stripped_$sobase"
 stripsym="/tmp/stripped_$sobase.sym"
+ustripsym="/tmp/uniq_stripped_$sobase.sym"
 
 readelf -s -W "$sofile" | awk -F' ' '{print $8}' > "$sosym"
 
-objcopy -v -g "$sofile" "$stripfile"
+objcopy -v -g --strip-unneeded "$sofile" "$stripfile"
 readelf -s -W "$stripfile" | awk -F' ' '{print $8}' > "$stripsym"
 
-diff -u "$sosym" "$stripsym"
+uniq "$sosym" "$usosym"
+uniq "$stripsym" "$ustripsym"
+while read line; do
+    grep -q "^${line}$" "$ustripsym" || echo "$line"
+done < "$usosym"
 
-rm -vf /tmp/*.so /tmp/*.sym
+rm -vf "$stripfile" "$sosym" "$usosym" "$stripsym" "$ustripsym"
