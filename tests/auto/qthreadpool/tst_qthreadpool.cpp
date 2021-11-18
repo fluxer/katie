b3d87c9b7d9993c1f065422631aaf729a03b2cad
Ivailo Monev <xakepa10@gmail.com>
2021-11-19 01:33:25 +0200

    de-duplicate QTRY_COMPARE() macro
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- tests/auto/qthreadpool/tst_qthreadpool.cpp
+++ tests/auto/qthreadpool/tst_qthreadpool.cpp
@@ -25,23 +25,7 @@
 #include <qstring.h>
 #include <qmutex.h>
 
-// From Qt5
-
-#define QTRY_COMPARE_WITH_TIMEOUT(__expr, __expected, __timeout) \
-    do { \
-            const int __step = 50; \
-            const int __timeoutValue = __timeout; \
-            if ((__expr) != (__expected)) { \
-                        QTest::qWait(0); \
-                    } \
-            for (int __i = 0; __i < __timeoutValue && ((__expr) != (__expected)); __i+=__step) { \
-                        QTest::qWait(__step); \
-                    } \
-            QCOMPARE(__expr, __expected); \
-    } while (0)
-
-#define QTRY_COMPARE(__expr, __expected) QTRY_COMPARE_WITH_TIMEOUT(__expr, __expected, 5000)
-
+#include "../../shared/util.h"
 
 typedef void (*FunctionPointer)();
 
