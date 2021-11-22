fluxer/katie

commit deff95c0f90e4d341e0d519c5d5576b2f1edeb32

get rid of QPicture
Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

src/gui/painting/qpaintengine_alpha.cpp
@@ -44,10 +44,7 @@
 #ifndef QT_NO_PRINTER
 #include <qdebug.h>
 #include "qpaintengine_alpha_p.h"
-
-#include "qpicture_p.h"
 #include "qfont_p.h"
-#include "QtGui/qpicture.h"
 
 QT_BEGIN_NAMESPACE
 
@@ -314,7 +311,7 @@ void QAlphaPaintEngine::flushAndInit(bool init)
 
         d->m_cliprgn = d->m_alphargn;
 
-        // now replay the QPicture
+        // now replay the QImage
         ++d->m_pass; // we are now doing pass #2
 
         // reset states
@@ -323,12 +320,12 @@ void QAlphaPaintEngine::flushAndInit(bool init)
         painter()->save();
         d->resetState(painter());
 
-        // make sure the output from QPicture is unscaled
+        // make sure the output from QImage is unscaled
         QTransform mtx;
         mtx.scale(1.0f / (qreal(d->m_pdev->logicalDpiX()) / qreal(qt_defaultDpiX())),
                   1.0f / (qreal(d->m_pdev->logicalDpiY()) / qreal(qt_defaultDpiY())));
         painter()->setTransform(mtx);
-        painter()->drawPicture(0, 0, *d->m_pic);
+        painter()->drawImage(0, 0, *d->m_pic);
 
         d->m_cliprgn = QRegion();
         d->resetState(painter());
@@ -349,8 +346,7 @@ void QAlphaPaintEngine::flushAndInit(bool init)
     if (init) {
         gccaps = PaintEngineFeatures(AllFeatures & ~QPaintEngine::ObjectBoundingModeGradients);
 
-        d->m_pic = new QPicture();
-        d->m_pic->d_ptr->in_memory_only = true;
+        d->m_pic = new QImage();
         d->m_picpainter = new QPainter(d->m_pic);
         d->m_picengine = d->m_picpainter->paintEngine();
 
@@ -476,7 +472,7 @@ void QAlphaPaintEnginePrivate::drawAlphaImage(const QRectF &rect)
             QPainter imgpainter(&img);
             imgpainter.setTransform(picscale);
             QPointF picpos(qreal(-xpos), qreal(-ypos));
-            imgpainter.drawPicture(picpos, *m_pic);
+            imgpainter.drawImage(picpos, *m_pic);
             imgpainter.end();
 
             q->painter()->setTransform(QTransform());

src/gui/painting/qpaintengine_preview.cpp
@@ -42,11 +42,9 @@
 #include <qpaintengine_preview_p.h>
 #include <qpainter_p.h>
 #include <qpaintengine_p.h>
-#include <qpicture_p.h>
 
 #include <QtGui/qprintengine.h>
 #include <QtGui/qpainter.h>
-#include <QtGui/qpicture.h>
 
 #ifndef QT_NO_PRINTPREVIEWWIDGET
 QT_BEGIN_NAMESPACE
@@ -58,7 +56,7 @@ public:
     QPreviewPaintEnginePrivate() : state(QPrinter::Idle) {}
     ~QPreviewPaintEnginePrivate() {}
 
-    QList<const QPicture *> pages;
+    QList<const QImage *> pages;
     QPaintEngine *engine;
     QPainter *painter;
     QPrinter::PrinterState state;
@@ -90,8 +88,7 @@ bool QPreviewPaintEngine::begin(QPaintDevice *)
     qDeleteAll(d->pages);
     d->pages.clear();
 
-    QPicture *page = new QPicture;
-    page->d_func()->in_memory_only = true;
+    QImage *page = new QImage;
     d->painter = new QPainter(page);
     d->engine = d->painter->paintEngine();
     d->pages.append(page);
@@ -150,8 +147,7 @@ bool QPreviewPaintEngine::newPage()
 {
     Q_D(QPreviewPaintEngine);
 
-    QPicture *page = new QPicture;
-    page->d_func()->in_memory_only = true;
+    QImage *page = new QImage;
     QPainter *tmp_painter = new QPainter(page);
     QPaintEngine *tmp_engine = tmp_painter->paintEngine();
 
@@ -181,7 +177,7 @@ bool QPreviewPaintEngine::abort()
     return true;
 }
 
-QList<const QPicture *> QPreviewPaintEngine::pages()
+QList<const QImage *> QPreviewPaintEngine::pages()
 {
     Q_D(QPreviewPaintEngine);
     return d->pages;

CMakeLists.txt
@@ -35,7 +35,7 @@ set(CPACK_PACKAGE_VERSION_MAJOR "4")
 set(CPACK_PACKAGE_VERSION_MINOR "9")
 set(CPACK_PACKAGE_VERSION_PATCH "0")
 set(CPACK_SET_DESTDIR TRUE)
-set(CPACK_SOURCE_IGNORE_FILES "/build/;/.git;${CPACK_SOURCE_IGNORE_FILES}")
+set(CPACK_SOURCE_IGNORE_FILES "/build/;/minsize/;/.git;${CPACK_SOURCE_IGNORE_FILES}")
 set(CPACK_STRIP_FILES TRUE)
 set(CPACK_GENERATOR "TXZ")
 set(CPACK_SOURCE_GENERATOR "TXZ")

README
@@ -27,7 +27,7 @@ There are several things you should be aware before considering Katie:
   - OpenVG is no longer supported graphics output
   - Blitter is no longer supported painting engine
   - native graphics system is no more, rasterizer is just as fast as native
-  - QPictureIO and plugins for it are no longer supported
+  - QPicture, QPictureIO and plugins for it are no longer supported
   - QML debugger and inspector have been removed, potentially dangerous
   - no SIMD, zero performance impact with decent compiler
   - state machine is no more

src/core/global/qconfig.h.cmake
@@ -49,6 +49,7 @@
 #define QT_NO_DIRECTPAINTER
 #define QT_NO_COP
 #define QT_NO_BLITTABLE
+#define QT_NO_PICTURE
 #define QT_NO_PICTUREIO
 #define QT_NO_SOFTKEYMANAGER
 #define QT_NO_SXE
@@ -225,7 +226,6 @@
 #cmakedefine QT_NO_NSL
 #cmakedefine QT_NO_PAINT_DEBUG
 #cmakedefine QT_NO_PDF
-#cmakedefine QT_NO_PICTURE
 #cmakedefine QT_NO_PLUGIN_CHECK
 #cmakedefine QT_NO_PRINTDIALOG
 #cmakedefine QT_NO_PRINTER
@@ -530,7 +530,7 @@
 #endif
 
 // QPrinter
-#if !defined(QT_NO_PRINTER) && (defined(QT_NO_TEXTSTREAM) || defined(QT_NO_PICTURE) || defined(QT_NO_TEMPORARYFILE))
+#if !defined(QT_NO_PRINTER) && (defined(QT_NO_TEXTSTREAM) || defined(QT_NO_TEMPORARYFILE))
 #define QT_NO_PRINTER
 #endif
 

src/core/global/qnamespace.h
@@ -1464,17 +1464,16 @@ typedef bool (*qInternalCallback)(void **);
 class Q_CORE_EXPORT QInternal {
 public:
     enum PaintDeviceFlags {
-        UnknownDevice = 0x00,
-        Widget        = 0x01,
-        Pixmap        = 0x02,
-        Image         = 0x03,
-        Printer       = 0x04,
-        Picture       = 0x05,
-        Pbuffer       = 0x06,    // GL pbuffer
-        FramebufferObject = 0x07, // GL framebuffer object
-        CustomRaster  = 0x08,
-        PaintBuffer   = 0x09,
-        OpenGL        = 0x0a
+        UnknownDevice     = 0,
+        Widget            = 1,
+        Pixmap            = 2,
+        Image             = 3,
+        Printer           = 4,
+        Pbuffer           = 5,    // GL pbuffer
+        FramebufferObject = 6, // GL framebuffer object
+        CustomRaster      = 7,
+        PaintBuffer       = 8,
+        OpenGL            = 9
     };
     enum RelayoutType {
         RelayoutNormal,

src/gui/CMakeLists.txt
@@ -169,7 +169,6 @@ set(GUI_PUBLIC_HEADERS
     QPalette
     QPanGesture
     QPen
-    QPicture
     QPixmap
     QPixmapCache
     QPlainTextDocumentLayout

src/gui/graphicsview/qgraphicsitem.cpp
@@ -5140,11 +5140,11 @@ QRegion QGraphicsItem::boundingRegion(const QTransform &itemToDeviceTransform) c
     // ### Ideally we would have a better way to generate this region,
     // preferably something in the lines of QPainterPath::toRegion(QTransform)
     // coupled with a way to generate a painter path from a set of painter
-    // operations (e.g., QPicture::toPainterPath() or so). The current
-    // approach generates a bitmap with the size of the item's bounding rect
-    // in device coordinates, scaled by b.r.granularity, then paints the item
-    // into the bitmap, converts the result to a QRegion and scales the region
-    // back to device space with inverse granularity.
+    // operations. The current approach generates a bitmap with the size of
+    // the item's bounding rect in device coordinates, scaled by
+    // b.r.granularity, then paints the item into the bitmap, converts the
+    // result to a QRegion and scales the region back to device space with
+    // inverse granularity.
     qreal granularity = boundingRegionGranularity();
     QRect deviceRect = itemToDeviceTransform.mapRect(boundingRect()).toRect();
     _q_adjustRect(&deviceRect);

commit b73f5fe064996ebbde6fc0e615a5973ce3a973b9

do not use alpha paint engine for postscript panting
fixes postscript print feature, regression since
deff95c0f90e4d341e0d519c5d5576b2f1edeb32

Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

src/gui/painting/qpaintengine_alpha.cpp
@@ -327,7 +327,8 @@ void QAlphaPaintEngine::flushAndInit(bool init)
     if (init) {
         gccaps = PaintEngineFeatures(AllFeatures & ~QPaintEngine::ObjectBoundingModeGradients);
 
-        d->m_pic = new QImage();
+        d->m_pic = new QImage(QSize(1024, 1024), QImage::Format_RGB32);
+        d->m_pic->fill(Qt::white);
         d->m_picpainter = new QPainter(d->m_pic);
         d->m_picengine = d->m_picpainter->paintEngine();
 

src/gui/painting/qpaintengine_preview.cpp
@@ -68,7 +68,7 @@ bool QPreviewPaintEngine::begin(QPaintDevice *)
     qDeleteAll(d->pages);
     d->pages.clear();
 
-    QImage *page = new QImage;
+    QImage *page = new QImage(QSize(1024, 1024), QImage::Format_RGB32);
     d->painter = new QPainter(page);
     d->engine = d->painter->paintEngine();
     d->pages.append(page);
@@ -127,7 +127,7 @@ bool QPreviewPaintEngine::newPage()
 {
     Q_D(QPreviewPaintEngine);
 
-    QImage *page = new QImage;
+    QImage *page = new QImage(QSize(1024, 1024), QImage::Format_RGB32);
     QPainter *tmp_painter = new QPainter(page);
     QPaintEngine *tmp_engine = tmp_painter->paintEngine();
 

src/gui/painting/qprintengine_ps.cpp
@@ -154,7 +154,7 @@ QPSPrintEnginePrivate::QPSPrintEnginePrivate(QPrinter::PrinterMode m)
     : QPdfBaseEnginePrivate(m),
       printerState(QPrinter::Idle), hugeDocument(false), headerDone(false)
 {
-    useAlphaEngine = true;
+    useAlphaEngine = false;
     postscript = true;
 
     firstPage = true;