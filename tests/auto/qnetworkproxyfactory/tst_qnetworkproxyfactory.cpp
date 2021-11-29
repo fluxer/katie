0f7c07f7fdd2490e9ffa31bc883498d191221a3f
Ivailo Monev <xakepa10@gmail.com>
2021-11-29 17:45:29 +0200

    compiler warning fix
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- tests/auto/qnetworkproxyfactory/tst_qnetworkproxyfactory.cpp
+++ tests/auto/qnetworkproxyfactory/tst_qnetworkproxyfactory.cpp
@@ -74,7 +74,7 @@ QString tst_QNetworkProxyFactory::formatProxyName(const QNetworkProxy & proxy) c
 {
     QString proxyName;
     if (!proxy.user().isNull())
-        proxyName.append("%1:%2@").arg(proxy.user(), proxy.password());
+        proxyName = QString::fromLatin1("%1:%2@").arg(proxy.user(), proxy.password());
     proxyName.append(QString("%1:%2").arg(proxy.hostName()).arg(proxy.port()));
     proxyName.append(QString(" (type=%1, capabilities=%2)").arg(proxy.type()).arg(proxy.capabilities()));
 
