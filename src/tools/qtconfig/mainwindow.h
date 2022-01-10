0c6e8c00b78df2afc83687876f6d8586ded98b48
Ivailo Monev <xakepa10@gmail.com>
2022-01-10 11:55:38 +0200

    remove raw fonts support leftovers
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/tools/qtconfig/mainwindow.h
+++ src/tools/qtconfig/mainwindow.h
@@ -47,11 +47,6 @@ public slots:
     virtual void paletteSelected(int);
     virtual void styleSelected(const QString &);
     virtual void familySelected(const QString &);
-    virtual void removeFontpath();
-    virtual void addFontpath();
-    virtual void downFontpath();
-    virtual void upFontpath();
-    virtual void browseFontpath();
     virtual void fileSave();
     virtual void fileExit();
     virtual void somethingModified();
@@ -75,7 +70,6 @@ private:
     QString desktopThemeName;
     QPalette editPalette, previewPalette;
     QStyle *previewstyle;
-    QStringList fontpaths;
     bool modified;
 };
 
