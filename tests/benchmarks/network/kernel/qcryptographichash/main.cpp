bfee83b9ab50505f7cf1a2cf410981c806ee6f8e
Ivailo Monev <xakepa10@gmail.com>
2022-03-11 04:48:25 +0200

    benchmark all QCryptographicHash algorithms
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- tests/benchmarks/network/kernel/qcryptographichash/main.cpp
+++ tests/benchmarks/network/kernel/qcryptographichash/main.cpp
@@ -1,7 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2015 The Qt Company Ltd.
-** Copyright (C) 2016 Ivailo Monev
+** Copyright (C) 2019 Ivailo Monev
 **
 ** This file is part of the test suite of the Katie Toolkit.
 **
@@ -27,13 +26,37 @@ QT_USE_NAMESPACE
 
 static const QByteArray lorem = QByteArray("Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.");
 
+Q_DECLARE_METATYPE(QCryptographicHash::Algorithm)
+
+static QString algorithmToString(const QCryptographicHash::Algorithm algorithm)
+{
+    switch (algorithm) {
+        case QCryptographicHash::Md5: {
+            return QString::fromLatin1("Md5");
+        }
+        case QCryptographicHash::Sha1: {
+            return QString::fromLatin1("Sha1");
+        }
+        case QCryptographicHash::Sha256: {
+            return QString::fromLatin1("Sha256");
+        }
+        case QCryptographicHash::Sha512: {
+            return QString::fromLatin1("Sha512");
+        }
+    }
+    Q_ASSERT(false);
+    return QString();
+}
+
 class tst_qcryptographichash : public QObject
 {
     Q_OBJECT
 private slots:
     void append_data();
     void append();
+    void append_once_data();
     void append_once();
+    void statichash_data();
     void statichash();
     void algorithms_data();
     void algorithms();
@@ -42,15 +65,20 @@ private slots:
 void tst_qcryptographichash::append_data()
 {
     QTest::addColumn<int>("size");
-    QTest::newRow("10")  << int(10);
-    QTest::newRow("100") << int(100);
-    QTest::newRow("250") << int(250);
-    QTest::newRow("500") << int(500);
+    QTest::addColumn<QCryptographicHash::Algorithm>("algorithm");
+    for (int i = QCryptographicHash::Md5; i < QCryptographicHash::Sha512; i++) {
+        const QCryptographicHash::Algorithm algorithm = static_cast<QCryptographicHash::Algorithm>(i);
+        QTest::newRow(QString::fromLatin1("10 (%1)").arg(algorithmToString(algorithm)).toAscii())  << int(10) << algorithm;
+        QTest::newRow(QString::fromLatin1("100 (%1)").arg(algorithmToString(algorithm)).toAscii()) << int(100) << algorithm;
+        QTest::newRow(QString::fromLatin1("250 (%1)").arg(algorithmToString(algorithm)).toAscii()) << int(250) << algorithm;
+        QTest::newRow(QString::fromLatin1("500 (%1)").arg(algorithmToString(algorithm)).toAscii()) << int(500) << algorithm;
+    }
 }
 
 void tst_qcryptographichash::append()
 {
     QFETCH(int, size);
+    QFETCH(QCryptographicHash::Algorithm, algorithm);
 
     const int chunksize = lorem.size() / size;
     QVERIFY(chunksize > 0);
@@ -62,7 +90,7 @@ void tst_qcryptographichash::append()
     }
 
     QBENCHMARK {
-        QCryptographicHash hash(QCryptographicHash::Sha512);
+        QCryptographicHash hash(algorithm);
         for (int i = 0; i < chunks.size(); i++) {
             hash.addData(chunks.at(i));
         }
@@ -70,32 +98,48 @@ void tst_qcryptographichash::append()
     }
 }
 
+void tst_qcryptographichash::append_once_data()
+{
+    QTest::addColumn<QCryptographicHash::Algorithm>("algorithm");
+    QTest::newRow("Md5")    << QCryptographicHash::Md5;
+    QTest::newRow("Sha1")   << QCryptographicHash::Sha1;
+    QTest::newRow("Sha256") << QCryptographicHash::Sha256;
+    QTest::newRow("Sha512") << QCryptographicHash::Sha512;
+}
+
 void tst_qcryptographichash::append_once()
 {
+    QFETCH(QCryptographicHash::Algorithm, algorithm);
+
     QBENCHMARK {
-        QCryptographicHash hash(QCryptographicHash::Sha512);
+        QCryptographicHash hash(algorithm);
         hash.addData(lorem);
         QVERIFY(!hash.result().isEmpty());
     }
 }
 
-void tst_qcryptographichash::statichash() {
+void tst_qcryptographichash::statichash_data()
+{
+    append_once_data();
+}
+
+void tst_qcryptographichash::statichash()
+{
+    QFETCH(QCryptographicHash::Algorithm, algorithm);
+
     QBENCHMARK {
-        QByteArray hash = QCryptographicHash::hash(lorem, QCryptographicHash::Sha512);
+        QByteArray hash = QCryptographicHash::hash(lorem, algorithm);
         QVERIFY(!hash.isEmpty());
     }
 }
 
 void tst_qcryptographichash::algorithms_data()
 {
-    QTest::addColumn<QCryptographicHash::Algorithm>("algorithm");
-    QTest::newRow("Md5")  << QCryptographicHash::Md5;
-    QTest::newRow("Sha1")  << QCryptographicHash::Sha1;
-    QTest::newRow("Sha256")  << QCryptographicHash::Sha256;
-    QTest::newRow("Sha512")  << QCryptographicHash::Sha512;
+    statichash_data();
 }
 
-void tst_qcryptographichash::algorithms() {
+void tst_qcryptographichash::algorithms()
+{
     QFETCH(QCryptographicHash::Algorithm, algorithm);
 
     QBENCHMARK {
@@ -105,7 +149,5 @@ void tst_qcryptographichash::algorithms() {
 }
 
 QTEST_MAIN(tst_qcryptographichash)
-
-Q_DECLARE_METATYPE(QCryptographicHash::Algorithm)
 
 #include "moc_main.cpp"
