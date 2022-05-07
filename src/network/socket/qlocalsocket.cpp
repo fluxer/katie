dd89588aa0603ba5e2290920eeabe394e71903e5
Ivailo Monev <xakepa10@gmail.com>
2022-05-07 21:22:54 +0300

    remove win leftovers
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/network/socket/qlocalsocket.cpp
+++ src/network/socket/qlocalsocket.cpp
@@ -32,7 +32,7 @@ QT_BEGIN_NAMESPACE
 
     \brief The QLocalSocket class provides a local socket.
 
-    On Windows this is a named pipe and on Unix this is a local domain socket.
+    On Unix this is a local domain socket.
 
     If an error occurs, socketError() returns the type of error, and
     errorString() can be called to get a human readable description
@@ -42,9 +42,6 @@ QT_BEGIN_NAMESPACE
     to use it without one. In that case, you must use waitForConnected(),
     waitForReadyRead(), waitForBytesWritten(), and waitForDisconnected()
     which blocks until the operation is complete or the timeout expires.
-
-    Note that this feature is not supported on versions of Windows earlier than
-    Windows XP.
 
     \sa QLocalServer
 */
