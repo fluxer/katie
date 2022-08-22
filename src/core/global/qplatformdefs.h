1d387f1ddf31d8a4a02969b2c778a6dc54eb66f4
Ivailo Monev <xakepa10@gmail.com>
2022-08-23 01:27:58 +0300

    remove now unused signal and socket system header inclusions in qplatformdefs header
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/core/global/qplatformdefs.h
+++ src/core/global/qplatformdefs.h
@@ -25,10 +25,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <dirent.h>
-#include <signal.h>
 #include <fcntl.h>
 #include <sys/types.h>
-#include <sys/socket.h>
 #include <sys/stat.h>
 
 // Definitions
